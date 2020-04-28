f601e1f390e5f7b3490040f41cd43e292b95fbfa
kexplx <oscar.rosner@web.de>
2020-04-29 00:06:50 +0200

    Fix "should create" unit test by providing NbMenuService to testbed

--- ng-frontend/src/app/project-list/project-card/project-card.component.spec.ts
+++ ng-frontend/src/app/project-list/project-card/project-card.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProjectCardComponent } from './project-card.component';
+import { NbMenuService } from '@nebular/theme';
 
 describe('ProjectCardComponent', () => {
   let component: ProjectCardComponent;
@@ -8,9 +9,9 @@ describe('ProjectCardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProjectCardComponent ]
-    })
-    .compileComponents();
+      declarations: [ProjectCardComponent],
+      providers: [NbMenuService],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
