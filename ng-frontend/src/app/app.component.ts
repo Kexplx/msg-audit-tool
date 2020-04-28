9dc43fe67457f20ec3da439828ab611a491b6b6c
kexplx <oscar.rosner@web.de>
2020-04-29 00:04:35 +0200

    Remove NbMenuService, moved to project-card component

--- ng-frontend/src/app/app.component.ts
+++ ng-frontend/src/app/app.component.ts
@@ -7,15 +7,4 @@ import { map } from 'rxjs/operators';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  items = [{ title: 'Löschen' }, { title: 'Infos' }];
-
-  constructor(private nbMenuService: NbMenuService) {}
-
-  ngOnInit() {
-    this.nbMenuService
-      .onItemClick()
-      .pipe(map(x => x.item.title))
-      .subscribe(x => console.log(x));
-  }
-}
+export class AppComponent {}
