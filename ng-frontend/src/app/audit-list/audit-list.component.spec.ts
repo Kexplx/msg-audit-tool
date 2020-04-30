6286d8cf275330350b5d35ffe6333f0364da8f2b
kexplx <oscar.rosner@web.de>
2020-04-30 18:12:22 +0200

    Fix tests for audit list

--- ng-frontend/src/app/audit-list/audit-list.component.spec.ts
+++ ng-frontend/src/app/audit-list/audit-list.component.spec.ts
@@ -1,21 +1,11 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AuditListComponent } from './audit-list.component';
 
 describe('AuditListComponent', () => {
   let component: AuditListComponent;
-  let fixture: ComponentFixture<AuditListComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [AuditListComponent],
-    }).compileComponents();
-  }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AuditListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    const dialogServiceStub = jasmine.createSpyObj('NbDialogService', ['open']);
+    component = new AuditListComponent(dialogServiceStub);
   });
 
   it('should create', () => {
