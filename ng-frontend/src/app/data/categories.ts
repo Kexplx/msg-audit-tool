dd668f2c6a6276361b11145dcd7bae13bcdc3eb6
kexplx <oscar.rosner@web.de>
2020-04-30 17:07:32 +0200

    Add data set for ISO categories

---
+++ ng-frontend/src/app/data/categories.ts
@@ -0,0 +1,18 @@
+import { Category } from './models/category.model';
+
+export const categories: Category[] = [
+  { title: '3. Zufriedenheit', subcategories: [{ title: '3. sdadskaakds' }] },
+  { title: '3. Zufriedenheit', subcategories: [{ title: '3. sdadskaakds' }] },
+  { title: '3. Zufriedenheit', subcategories: [{ title: '3. sdadskaakds' }] },
+  { title: '3. Zufriedenheit', subcategories: [{ title: '3. sdadskaakds' }] },
+  { title: '3. Zufriedenheit', subcategories: [{ title: '3. sdadskaakds' }] },
+  { title: '3. Zufriedenheit', subcategories: [{ title: '3. sdadskaakds' }] },
+  { title: '3. Zufriedenheit', subcategories: [{ title: '3. sdadskaakds' }] },
+  { title: '3. Zufriedenheit', subcategories: [{ title: '3. sdadskaakds' }] },
+  { title: '3. Zufriedenheit', subcategories: [{ title: '3. sdadskaakds' }] },
+  { title: '3. Zufriedenheit', subcategories: [{ title: '3. sdadskaakds' }] },
+  { title: '3. Zufriedenheit', subcategories: [{ title: '3. sdadskaakds' }] },
+  { title: '3. Zufriedenheit', subcategories: [{ title: '3. sdadskaakds' }] },
+  { title: '3. Zufriedenheit', subcategories: [{ title: '3. sdadskaakds' }] },
+  { title: '3. Zufriedenheit', subcategories: [{ title: '3. sdadskaakds' }] },
+];
