bd5805153a20f32fdcbe507ca23452ad093f756f
kexplx <oscar.rosner@web.de>
2020-04-30 17:18:47 +0200

    Add data set for iso categories

--- ng-frontend/src/app/data/iso-categories.ts
+++ ng-frontend/src/app/data/iso-categories.ts
@@ -1,6 +1,6 @@
-import { Category } from './models/category.model';
+import { IsoCategory } from './models/iso-category.model';
 
-export const categories: Category[] = [
+export const categories: IsoCategory[] = [
   { title: '3. Zufriedenheit', subcategories: [{ title: '3. sdadskaakds' }] },
   { title: '3. Zufriedenheit', subcategories: [{ title: '3. sdadskaakds' }] },
   { title: '3. Zufriedenheit', subcategories: [{ title: '3. sdadskaakds' }] },
