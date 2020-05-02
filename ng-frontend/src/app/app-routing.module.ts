a0cb95a15413a12e362dcaef686a90b96a670366
kexplx <oscar.rosner@web.de>
2020-05-02 16:29:05 +0200

    Add wildcard route to routing module

--- ng-frontend/src/app/app-routing.module.ts
+++ ng-frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuditListComponent } from './audit-list/audit-list.component';
 import { AddAuditComponent } from './add-audit/add-audit.component';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'audits' },
   { path: 'audits', component: AuditListComponent, children: [] },
   { path: 'audits/add', component: AddAuditComponent },
+  { path: '**', component: ErrorComponent },
 ];
 
 @NgModule({
