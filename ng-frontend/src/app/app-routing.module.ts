4a2181778c78edc73c3eac36ea4e23f687b58e72
kexplx <oscar.rosner@web.de>
2020-05-02 16:06:04 +0200

    Add /add route

--- ng-frontend/src/app/app-routing.module.ts
+++ ng-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuditListComponent } from './audit-list/audit-list.component';
+import { AddAuditComponent } from './add-audit/add-audit.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'audits' },
-  { path: 'audits', component: AuditListComponent },
+  { path: 'audits', component: AuditListComponent, children: [] },
+  { path: 'audits/add', component: AddAuditComponent },
 ];
 
 @NgModule({
