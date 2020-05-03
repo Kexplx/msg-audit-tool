b4f3ec4f909209e94a895386d433bc9bfdc8f4a2
kexplx <oscar.rosner@web.de>
2020-05-03 15:37:26 +0200

    Add form for capturing iso categories

---
+++ ng-frontend/src/app/add-audit/category-form/category-form.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { isoCategories } from 'src/app/data/iso-categories';
+import { IsoCategory } from 'src/app/data/models/iso-category.model';
+
+@Component({
+  selector: 'app-category-form',
+  templateUrl: './category-form.component.html',
+  styleUrls: ['./category-form.component.scss'],
+})
+export class CategoryFormComponent implements OnInit {
+  @Output() categoriesSubmitted = new EventEmitter<IsoCategory[]>();
+  categories: IsoCategory[];
+  selectedCategoriesTitles: IsoCategory[];
+
+  ngOnInit(): void {
+    this.categories = isoCategories;
+    this.selectedCategoriesTitles = [];
+  }
+
+  addParentCategory(parentCategory: IsoCategory) {
+    this.selectedCategoriesTitles.push({ ...parentCategory });
+  }
+
+  removeParentCategory(parentCategory: IsoCategory) {
+    const index = this.selectedCategoriesTitles.indexOf(parentCategory);
+    this.selectedCategoriesTitles.splice(index, 1);
+  }
+
+  addChildCategory(childCategory: IsoCategory, parentCategory: IsoCategory) {
+    const parent = this.selectedCategoriesTitles.find(x => x.title == parentCategory.title);
+
+    if (!parent) {
+      this.selectedCategoriesTitles.push({
+        title: parentCategory.title,
+        children: [childCategory],
+      });
+    } else {
+      parent.children.push(childCategory);
+    }
+  }
+
+  removeChildCategory(childCategory: IsoCategory, parentCategory: IsoCategory) {
+    parentCategory = this.selectedCategoriesTitles.find(x => x.title == parentCategory.title);
+    parentCategory.children = parentCategory.children.filter(x => x != childCategory);
+  }
+
+  isChecked(title: string) {
+    return (
+      this.selectedCategoriesTitles.find(
+        x => x.title == title || x.children?.map(x => x.title).includes(title),
+      ) != undefined
+    );
+  }
+
+  onSubmit() {
+    this.categoriesSubmitted.emit(
+      this.selectedCategoriesTitles.sort((a: IsoCategory, b: IsoCategory) =>
+        a.title > b.title ? 1 : -1,
+      ),
+    );
+  }
+}
