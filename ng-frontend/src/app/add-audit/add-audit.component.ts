69b85dfca894697e03906b88d3429f374cf6dfee
kexplx <oscar.rosner@web.de>
2020-05-03 15:36:59 +0200

    Implement reactive form for capturing audit base data

--- ng-frontend/src/app/add-audit/add-audit.component.ts
+++ ng-frontend/src/app/add-audit/add-audit.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Audit } from '../data/models/audit.model';
+import { IsoCategory } from '../data/models/iso-category.model';
+import { isoCategories } from '../data/iso-categories';
+import { Store } from '@ngxs/store';
+import { AddAudit } from '../ngxs/audit.actions';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-audit',
@@ -6,13 +13,95 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-audit.component.scss'],
 })
 export class AddAuditComponent implements OnInit {
-  isMobileScreen = false;
-  mediaMatcher = matchMedia('(max-width: 600px)');
+  auditForm: FormGroup;
+  audit: Audit;
+  isoCategories: { checked: boolean; category: IsoCategory }[];
 
-  ngOnInit(): void {
-    this.isMobileScreen = this.mediaMatcher.matches;
-    this.mediaMatcher.addEventListener('change', () => {
-      this.isMobileScreen = this.mediaMatcher.matches;
+  constructor(private store: Store, private router: Router) {}
+
+  get auditName() {
+    return this.auditForm.get('auditName');
+  }
+
+  get start() {
+    return this.auditForm.get('start');
+  }
+
+  get end() {
+    return this.auditForm.get('end');
+  }
+
+  get companyName() {
+    return this.auditForm.get('companyName');
+  }
+
+  get sector() {
+    return this.auditForm.get('sector');
+  }
+
+  get department() {
+    return this.auditForm.get('department');
+  }
+
+  get title() {
+    return this.auditForm.get('title');
+  }
+
+  get firstName() {
+    return this.auditForm.get('firstName');
+  }
+
+  get lastName() {
+    return this.auditForm.get('lastName');
+  }
+
+  get contactInformation() {
+    return this.auditForm.get('contactInformation');
+  }
+
+  parseDate(s: string) {
+    if (s) {
+      return new Date(s).getTime();
+    }
+  }
+
+  ngOnInit() {
+    this.auditForm = new FormGroup({
+      auditName: new FormControl(null, Validators.required),
+      start: new FormControl(null),
+      end: new FormControl(null),
+      companyName: new FormControl(null, Validators.required),
+      sector: new FormControl(null, Validators.required),
+      department: new FormControl(null, Validators.required),
+      title: new FormControl(null, Validators.required),
+      firstName: new FormControl(null, Validators.required),
+      lastName: new FormControl(null, Validators.required),
+      contactInformation: new FormControl(null, Validators.required),
     });
+    this.isoCategories = isoCategories.map(x => ({ category: x, checked: false }));
+  }
+
+  onSubmit(categories: IsoCategory[]) {
+    const audit: Audit = {
+      name: this.auditName.value,
+      contactPerson: {
+        firstName: this.firstName.value,
+        lastName: this.lastName.value,
+        title: this.title.value,
+        information: this.contactInformation.value,
+      },
+      customerData: {
+        department: this.department.value,
+        name: this.companyName.value,
+        sector: this.sector.value,
+      },
+      start: this.parseDate(this.start.value),
+      end: this.parseDate(this.end.value),
+      categories,
+    };
+
+    console.log(audit);
+
+    this.store.dispatch(new AddAudit(audit)).subscribe(() => this.router.navigate(['/audits']));
   }
 }
