45b96ec86ec23973b2f0c862c8a7213dea95c7b1
kexplx <oscar.rosner@web.de>
2020-05-03 16:00:36 +0200

    Import ngxs and routing module to testbed

--- ng-frontend/src/app/add-audit/add-audit.component.spec.ts
+++ ng-frontend/src/app/add-audit/add-audit.component.spec.ts
@@ -1,19 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddAuditComponent } from './add-audit.component';
+import { NgxsModule, Store } from '@ngxs/store';
+import { AuditRegistryState } from '../ngxs/audit-registry.state';
+import { Router, RouterModule } from '@angular/router';
 
 describe('AddAuditComponent', () => {
   let component: AddAuditComponent;
   let fixture: ComponentFixture<AddAuditComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AddAuditComponent ]
-    })
-    .compileComponents();
-  }));
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AddAuditComponent],
+      imports: [NgxsModule.forRoot([AuditRegistryState]), RouterModule.forRoot([])],
+    });
+
     fixture = TestBed.createComponent(AddAuditComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
