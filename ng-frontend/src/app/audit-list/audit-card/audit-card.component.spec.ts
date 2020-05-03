5ea91d92a3e55dcf6ba8a48234d7774e335cf5e1
kexplx <oscar.rosner@web.de>
2020-05-04 00:04:35 +0200

    Import NbModule to testbed and adds test for checking if audit namer renders to card header

--- ng-frontend/src/app/audit-list/audit-card/audit-card.component.spec.ts
+++ ng-frontend/src/app/audit-list/audit-card/audit-card.component.spec.ts
@@ -1,26 +1,51 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AuditCardComponent } from './audit-card.component';
-import { NbMenuService } from '@nebular/theme';
+import { Audit } from 'src/app/data/models/audit.model';
+import { AppNebularModule } from 'src/app/app-nebular.module';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DebugElement } from '@angular/core';
 
 describe('AuditCardComponent', () => {
   let component: AuditCardComponent;
   let fixture: ComponentFixture<AuditCardComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [AuditCardComponent],
-      providers: [NbMenuService],
-    }).compileComponents();
-  }));
+  const audit: Audit = {
+    name: 'TestAudit',
+    contactPerson: {
+      firstName: 'Max',
+      lastName: 'Meier',
+      information: '0192',
+      title: 'Herr',
+    },
+    customerData: {
+      department: 'a',
+      name: 'b',
+      sector: 'c',
+    },
+  };
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AuditCardComponent],
+      imports: [RouterModule.forRoot([]), AppNebularModule, BrowserAnimationsModule],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(AuditCardComponent);
     component = fixture.componentInstance;
+
+    component.audit = audit;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render audit name into nb-accordion-item-header-wrapper', () => {
+    const de: DebugElement = fixture.debugElement;
+    const el: HTMLElement = de.nativeElement.querySelector('.nb-accordion-item-header-wrapper');
+    expect(el.innerHTML).toContain(audit.name);
   });
 });
