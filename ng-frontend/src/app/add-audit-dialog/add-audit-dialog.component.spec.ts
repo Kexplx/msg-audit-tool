aa5019b8e8d064e0e03a9e286737d36ea3f63449
kexplx <oscar.rosner@web.de>
2020-05-04 14:01:14 +0200

    Add stubs for Router and dialog ref

--- ng-frontend/src/app/add-audit-dialog/add-audit-dialog.component.spec.ts
+++ ng-frontend/src/app/add-audit-dialog/add-audit-dialog.component.spec.ts
@@ -1,18 +1,48 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddAuditDialogComponent } from './add-audit-dialog.component';
+import { AppNebularModule } from '../app-nebular.module';
+import { RouterModule, Router } from '@angular/router';
+import { NgxsModule } from '@ngxs/store';
+import { AuditRegistryState } from '../ngxs/audit-registry.state';
+import { NbDialogRef } from '@nebular/theme';
+import { of } from 'rxjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AddAuditDialogComponent', () => {
   let component: AddAuditDialogComponent;
   let fixture: ComponentFixture<AddAuditDialogComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [AddAuditDialogComponent],
-    }).compileComponents();
-  }));
+  let nbDialogRefStub: Partial<NbDialogRef<any>>;
+  let nbRouterStub: Partial<Router>;
 
   beforeEach(() => {
+    nbDialogRefStub = {
+      onClose: of(true),
+      close: () => {},
+    };
+
+    nbRouterStub = {
+      navigate: () => of(true).toPromise(),
+    };
+
+    TestBed.configureTestingModule({
+      declarations: [AddAuditDialogComponent],
+      imports: [
+        AppNebularModule,
+        BrowserAnimationsModule,
+        RouterModule.forRoot([]),
+        NgxsModule.forRoot([AuditRegistryState]),
+      ],
+      providers: [
+        { provide: NbDialogRef, useValue: nbDialogRefStub },
+        {
+          provide: Router,
+          useValue: nbRouterStub,
+        },
+      ],
+    });
+
     fixture = TestBed.createComponent(AddAuditDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
