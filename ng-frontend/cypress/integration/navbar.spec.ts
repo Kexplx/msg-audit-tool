1ad0940ce1bac36a5ec4f53790e0aa365c40e77e
kexplx <oscar.rosner@web.de>
2020-05-06 01:41:17 +0200

    Add e2e tests for navbar

---
+++ ng-frontend/cypress/integration/navbar.spec.ts
@@ -0,0 +1,23 @@
+describe('Navbar', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4200');
+  });
+
+  it('Contains Menu Icon Button', () => {
+    cy.get(':nth-child(1) > .appearance-ghost');
+  });
+
+  it('Contains "Audit"', () => {
+    cy.get(':nth-child(2) > .appearance-hero');
+  });
+
+  it('Contains GitHub Icon Button', () => {
+    cy.get(':nth-child(4) > .appearance-ghost');
+  });
+
+  it('Clicking the sidebar toggle button opens the sidebar', () => {
+    cy.viewport('iphone-5');
+    cy.get(':nth-child(1) > .appearance-ghost').click();
+    cy.get('nb-sidebar').should('have.class', 'expanded');
+  });
+});
