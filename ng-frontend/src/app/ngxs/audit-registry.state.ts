80a30f2a96cdb3a5ef12ccf45cc167ae1c476017
kexplx <oscar.rosner@web.de>
2020-05-05 17:00:05 +0200

    Implement deleteAction

--- ng-frontend/src/app/ngxs/audit-registry.state.ts
+++ ng-frontend/src/app/ngxs/audit-registry.state.ts
@@ -1,7 +1,7 @@
 import { Audit } from '../data/models/audit.model';
 import { State, Selector, Action, StateContext } from '@ngxs/store';
 import { Injectable } from '@angular/core';
-import { AddAudit } from './audit.actions';
+import { AddAudit, DeleteAudit } from './audit.actions';
 
 export interface AuditRegistryStateModel {
   audits: Audit[];
@@ -21,12 +21,25 @@ export class AuditRegistryState {
   }
 
   @Action(AddAudit)
-  addStudent(context: StateContext<AuditRegistryStateModel>, { audit }: AddAudit) {
+  addAudit(context: StateContext<AuditRegistryStateModel>, { audit }: AddAudit) {
     const state = context.getState();
 
     context.setState({
       ...state,
       audits: [...state.audits, audit],
     });
+  }
+
+  @Action(DeleteAudit)
+  deleteAudit(context: StateContext<AuditRegistryStateModel>, { audit }: DeleteAudit) {
+    const state = context.getState();
+    const indexOfAudit = state.audits.indexOf(audit);
+
+    if (indexOfAudit != -1) {
+      context.setState({
+        ...state,
+        audits: [...state.audits.slice(0, indexOfAudit), ...state.audits.slice(indexOfAudit + 1)],
+      });
+    }
   }
 }
