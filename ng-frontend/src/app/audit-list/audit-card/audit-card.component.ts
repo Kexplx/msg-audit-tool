8697b6204d43bb99d69fe88fcd94d2491ca194ad
kexplx <oscar.rosner@web.de>
2020-05-05 18:41:33 +0200

    Dispatch delete action when contextmenu option "Delete" was clicked

--- ng-frontend/src/app/audit-list/audit-card/audit-card.component.ts
+++ ng-frontend/src/app/audit-list/audit-card/audit-card.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { NbMenuService, NbMenuItem } from '@nebular/theme';
-import { map } from 'rxjs/operators';
+import { NbMenuService, NbMenuItem, NbMenuBag } from '@nebular/theme';
+import { map, filter } from 'rxjs/operators';
 import { Audit } from 'src/app/data/models/audit.model';
 import { Store } from '@ngxs/store';
 import { DeleteAudit } from 'src/app/ngxs/audit.actions';
+import * as shortid from 'shortid';
 
 enum MenuOptions {
   Edit,
@@ -17,6 +18,7 @@ enum MenuOptions {
 })
 export class AuditCardComponent implements OnInit {
   @Input() audit: Audit;
+  nbMenuId: string;
   items: NbMenuItem[] = [
     { title: 'Bearbeiten', icon: 'edit-outline', data: MenuOptions.Edit },
     { title: 'Löschen', icon: 'trash-outline', data: MenuOptions.Delete },
@@ -25,9 +27,14 @@ export class AuditCardComponent implements OnInit {
   constructor(private nbMenuService: NbMenuService, private store: Store) {}
 
   ngOnInit() {
+    this.nbMenuId = shortid.generate();
+
     this.nbMenuService
       .onItemClick()
-      .pipe(map(menuBag => menuBag.item.data))
+      .pipe(
+        filter(({ tag }) => tag === this.nbMenuId),
+        map((menuBag: NbMenuBag) => menuBag.item.data),
+      )
       .subscribe((option: MenuOptions) => {
         switch (option) {
           case MenuOptions.Edit:
