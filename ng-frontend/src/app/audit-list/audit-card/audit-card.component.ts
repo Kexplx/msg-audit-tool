c28fd78202f4ac390253fc62cbf437a2235293d5
kexplx <oscar.rosner@web.de>
2020-05-05 16:59:51 +0200

    Implement deletion of audit when menuoption is clicked

--- ng-frontend/src/app/audit-list/audit-card/audit-card.component.ts
+++ ng-frontend/src/app/audit-list/audit-card/audit-card.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit, Input } from '@angular/core';
 import { NbMenuService, NbMenuItem } from '@nebular/theme';
 import { map } from 'rxjs/operators';
 import { Audit } from 'src/app/data/models/audit.model';
+import { Store } from '@ngxs/store';
+import { DeleteAudit } from 'src/app/ngxs/audit.actions';
+
+enum MenuOptions {
+  Edit,
+  Delete,
+}
 
 @Component({
   selector: 'app-audit-card',
@@ -11,16 +18,25 @@ import { Audit } from 'src/app/data/models/audit.model';
 export class AuditCardComponent implements OnInit {
   @Input() audit: Audit;
   items: NbMenuItem[] = [
-    { title: 'Bearbeiten', icon: 'edit-outline' },
-    { title: 'Löschen', icon: 'trash-outline' },
+    { title: 'Bearbeiten', icon: 'edit-outline', data: MenuOptions.Edit },
+    { title: 'Löschen', icon: 'trash-outline', data: MenuOptions.Delete },
   ];
 
-  constructor(private nbMenuService: NbMenuService) {}
+  constructor(private nbMenuService: NbMenuService, private store: Store) {}
 
   ngOnInit() {
     this.nbMenuService
       .onItemClick()
-      .pipe(map(x => x.item.title))
-      .subscribe(x => console.log(x));
+      .pipe(map(menuBag => menuBag.item.data))
+      .subscribe((option: MenuOptions) => {
+        switch (option) {
+          case MenuOptions.Edit:
+            //
+            break;
+          case MenuOptions.Delete:
+            this.store.dispatch(new DeleteAudit(this.audit));
+            break;
+        }
+      });
   }
 }
