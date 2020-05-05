c845518691e93a26c92a5bc9d255b46759e92dbf
kexplx <oscar.rosner@web.de>
2020-05-06 01:59:50 +0200

    Update AddAuditForm routing e2e test

--- ng-frontend/cypress/integration/add-audit-form.spec.ts
+++ ng-frontend/cypress/integration/add-audit-form.spec.ts
@@ -1,7 +1,7 @@
 describe('AddAuditForm', () => {
   let baseUrl = 'http://localhost:4200/audits/';
 
-  it('Route audits/neu opens AddAuditForm', () => {
+  it('Routing to audits/neu opens AddAuditForm', () => {
     cy.visit(baseUrl + 'neu');
     cy.get('#cdk-overlay-0 > nb-dialog-container > app-add-audit-dialog > div > form');
   });
