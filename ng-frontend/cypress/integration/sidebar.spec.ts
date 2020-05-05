1571aef4d0e863362aa386922395844d4d79973d
kexplx <oscar.rosner@web.de>
2020-05-06 01:41:33 +0200

    Add e2e tests for sidebar

---
+++ ng-frontend/cypress/integration/sidebar.spec.ts
@@ -0,0 +1,19 @@
+describe('Navbar', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4200');
+  });
+
+  it('Contains "Sidebar"', () => {
+    cy.get('.sidebar-container-content').contains('Sidebar');
+  });
+
+  it('Is hidden on initial load on mobile', () => {
+    cy.viewport('iphone-5');
+    cy.get('nb-sidebar').should('have.class', 'collapsed');
+  });
+
+  it('Is fixed on mobile', () => {
+    cy.viewport('iphone-5');
+    cy.get('nb-sidebar').should('have.class', 'fixed');
+  });
+});
