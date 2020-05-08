4f41df93ad732939037bc927a876f3929b269e1d
kexplx <oscar.rosner@web.de>
2020-05-08 21:26:08 +0200

    Add child routes add and edit to /audits

--- ng-frontend/src/app/app-routing.module.ts
+++ ng-frontend/src/app/app-routing.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuditListComponent } from './audit-list/audit-list.component';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
+import { AddAuditDialogComponent } from './shared/dialogs/add-audit-dialog/add-audit-dialog.component';
+import { EditAuditDialogComponent } from './shared/dialogs/edit-audit-dialog/edit-audit-dialog.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'audits' },
-  { path: 'audits', component: AuditListComponent },
-  { path: 'audits/neu', component: AuditListComponent },
-  { path: 'audits/:id/edit', component: AuditListComponent },
+  {
+    path: 'audits',
+    component: AuditListComponent,
+    children: [
+      { path: 'new', component: AddAuditDialogComponent },
+      { path: ':id/edit', component: EditAuditDialogComponent },
+    ],
+  },
   { path: '**', component: NotFoundComponent },
 ];
 
