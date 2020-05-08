fcd99c4e0a942c7ab7dc7124abf349264b9896e8
kexplx <oscar.rosner@web.de>
2020-05-08 21:16:23 +0200

    Provide stub for NbDialogService to TestBed

--- ng-frontend/src/app/shared/dialogs/add-audit-dialog/add-audit-dialog.component.spec.ts
+++ ng-frontend/src/app/shared/dialogs/add-audit-dialog/add-audit-dialog.component.spec.ts
@@ -1,16 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddAuditDialogComponent } from './add-audit-dialog.component';
-import { AppNebularModule } from '../app-nebular.module';
 import { RouterModule, Router } from '@angular/router';
 import { NgxsModule } from '@ngxs/store';
-import { AuditRegistryState } from '../ngxs/audit-registry.state';
-import { NbDialogRef } from '@nebular/theme';
+import { NbDialogRef, NbDialogService } from '@nebular/theme';
 import { of } from 'rxjs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AppNebularModule } from 'src/app/app-nebular.module';
+import { AuditRegistryState } from 'src/app/ngxs/audit-registry.state';
+import { EditAuditDialogComponent } from '../edit-audit-dialog/edit-audit-dialog.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('AddAuditDialogComponent', () => {
+fdescribe('AddAuditDialogComponent', () => {
   let component: AddAuditDialogComponent;
   let fixture: ComponentFixture<AddAuditDialogComponent>;
 
@@ -18,30 +20,21 @@ describe('AddAuditDialogComponent', () => {
   let nbRouterStub: Partial<Router>;
 
   beforeEach(() => {
-    nbDialogRefStub = {
+    const nbDialogRefStub: Partial<NbDialogRef<any>> = {
       onClose: of(true),
       close: () => {},
     };
 
-    nbRouterStub = {
-      navigate: () => of(true).toPromise(),
+    const nbDialogServiceStub = {
+      open: (...k: any) => nbDialogRefStub,
     };
 
     TestBed.configureTestingModule({
-      declarations: [AddAuditDialogComponent],
-      imports: [
-        AppNebularModule,
-        BrowserAnimationsModule,
-        RouterModule.forRoot([]),
-        NgxsModule.forRoot([AuditRegistryState]),
-        ReactiveFormsModule,
-      ],
+      declarations: [EditAuditDialogComponent],
+      imports: [AppNebularModule, NgxsModule.forRoot([AuditRegistryState]), RouterTestingModule],
       providers: [
         { provide: NbDialogRef, useValue: nbDialogRefStub },
-        {
-          provide: Router,
-          useValue: nbRouterStub,
-        },
+        { provide: NbDialogService, useValue: nbDialogServiceStub },
       ],
     });
 
