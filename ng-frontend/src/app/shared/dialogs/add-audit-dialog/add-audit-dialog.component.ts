7da7b3ec54b576759764bd14a379e669aea5981a
kexplx <oscar.rosner@web.de>
2020-05-08 21:26:55 +0200

    Obtain an instance of nbDialogref from viewchild

--- ng-frontend/src/app/shared/dialogs/add-audit-dialog/add-audit-dialog.component.ts
+++ ng-frontend/src/app/shared/dialogs/add-audit-dialog/add-audit-dialog.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { NbDialogRef } from '@nebular/theme';
+import { Component, OnInit, ViewChild, TemplateRef, AfterViewInit } from '@angular/core';
+import { NbDialogRef, NbDialogService } from '@nebular/theme';
 import { Router } from '@angular/router';
 import { Store } from '@ngxs/store';
 import { Audit } from 'src/app/data/models/audit.model';
 import { AddAudit } from 'src/app/ngxs/audit.actions';
+import { defaultDialogOptions } from '../default-dialog-options';
 
 @Component({
   selector: 'app-add-audit-dialog',
   templateUrl: './add-audit-dialog.component.html',
   styleUrls: ['./add-audit-dialog.component.scss'],
 })
-export class AddAuditDialogComponent implements OnInit {
-  constructor(private store: Store, private dialogRef: NbDialogRef<any>, private router: Router) {}
+export class AddAuditDialogComponent implements AfterViewInit {
+  @ViewChild('dialog') dialog: TemplateRef<any>;
+  dialogRef: NbDialogRef<any>;
 
-  ngOnInit(): void {
+  constructor(
+    private dialogService: NbDialogService,
+    private store: Store,
+    private router: Router,
+  ) {}
+
+  ngAfterViewInit() {
+    this.dialogRef = this.dialogService.open(this.dialog, defaultDialogOptions);
     this.dialogRef.onClose.subscribe(() => {
       this.router.navigate(['/audits']);
     });
--- ng-frontend/src/app/shared/dialogs/edit-audit-dialog/edit-audit-dialog.component.ts
+++ ng-frontend/src/app/shared/dialogs/edit-audit-dialog/edit-audit-dialog.component.ts
@@ -1,31 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { NbDialogRef } from '@nebular/theme';
+import { Component, OnInit, TemplateRef, ViewChild, AfterViewInit } from '@angular/core';
+import { NbDialogRef, NbDialogService } from '@nebular/theme';
 import { Store } from '@ngxs/store';
 import { AuditRegistryState } from 'src/app/ngxs/audit-registry.state';
 import { Observable } from 'rxjs';
 import { Audit } from 'src/app/data/models/audit.model';
 import { UpdateAudit } from 'src/app/ngxs/audit.actions';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { tap } from 'rxjs/operators';
+import { defaultDialogOptions } from '../default-dialog-options';
 
 @Component({
   selector: 'app-edit-audit-dialog',
   templateUrl: './edit-audit-dialog.component.html',
   styleUrls: ['./edit-audit-dialog.component.scss'],
 })
-export class EditAuditDialogComponent implements OnInit {
+export class EditAuditDialogComponent implements OnInit, AfterViewInit {
+  @ViewChild('dialog') dialog: TemplateRef<any>;
+  dialogRef: NbDialogRef<any>;
+
   audit$: Observable<Audit>;
   id: string;
 
   constructor(
-    protected dialogRef: NbDialogRef<{ id: string }>,
+    private dialogService: NbDialogService,
     private store: Store,
     private router: Router,
+    private route: ActivatedRoute,
   ) {}
 
   ngOnInit(): void {
-    this.id = this.dialogRef.componentRef.instance.id;
+    this.route.params.subscribe((params: Params) => {
+      this.id = params.id;
+    });
+
     this.audit$ = this.store.select(AuditRegistryState.audit(this.id));
+  }
+
+  ngAfterViewInit() {
+    this.dialogRef = this.dialogService.open(this.dialog, defaultDialogOptions);
+
     this.dialogRef.onClose.subscribe(() => {
       this.router.navigate(['/audits']);
     });
