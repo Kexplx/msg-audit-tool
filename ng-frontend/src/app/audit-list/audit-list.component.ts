9f934d158cbdc0140c6869f60c41ba41a6134f77
kexplx <oscar.rosner@web.de>
2020-05-08 21:48:51 +0200

    Remove custom routing in onInit

--- ng-frontend/src/app/audit-list/audit-list.component.ts
+++ ng-frontend/src/app/audit-list/audit-list.component.ts
@@ -3,16 +3,6 @@ import { Select } from '@ngxs/store';
 import { AuditRegistryState } from '../ngxs/audit-registry.state';
 import { Observable } from 'rxjs';
 import { Audit } from '../data/models/audit.model';
-import { NbDialogService, NbDialogConfig } from '@nebular/theme';
-import { ActivatedRoute, Router, UrlSegment } from '@angular/router';
-import { EditAuditDialogComponent } from '../shared/dialogs/edit-audit-dialog/edit-audit-dialog.component';
-import { AddAuditDialogComponent } from '../shared/dialogs/add-audit-dialog/add-audit-dialog.component';
-
-const defaultDialogOptions: Partial<NbDialogConfig> = {
-  autoFocus: false,
-  closeOnBackdropClick: false,
-  closeOnEsc: false,
-};
 
 @Component({
   selector: 'app-audit-list',
@@ -22,28 +12,5 @@ const defaultDialogOptions: Partial<NbDialogConfig> = {
 export class AuditListComponent implements OnInit {
   @Select(AuditRegistryState.audits) audits$: Observable<Audit[]>;
 
-  constructor(private dialogService: NbDialogService, private route: ActivatedRoute) {}
-
-  ngOnInit() {
-    this.route.url.subscribe((urlsegments: UrlSegment[]) => {
-      const url = urlsegments
-        .map(x => x.path)
-        .toString()
-        .replace(/,/g, '/');
-      switch (true) {
-        case /audits\/neu/.test(url):
-          this.dialogService.open(AddAuditDialogComponent, {
-            ...defaultDialogOptions,
-          });
-          break;
-        case /audits\/.*\/edit/.test(url):
-          this.dialogService.open(EditAuditDialogComponent, {
-            ...defaultDialogOptions,
-            context: { id: urlsegments[1].path },
-          });
-        default:
-          break;
-      }
-    });
-  }
+  ngOnInit() {}
 }
