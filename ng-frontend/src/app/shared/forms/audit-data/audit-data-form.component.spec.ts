ca6aeecebdaf2da055a5ba0e440d769466dd09d5
kexplx <oscar.rosner@web.de>
2020-05-08 02:18:56 +0200

    Import AppNebularModule to testbed

--- ng-frontend/src/app/shared/forms/audit-data/audit-data-form.component.spec.ts
+++ ng-frontend/src/app/shared/forms/audit-data/audit-data-form.component.spec.ts
@@ -1,18 +1,22 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AuditDataFormComponent } from './audit-data-form.component';
+import { FormBuilder } from '@angular/forms';
+import { AppNebularModule } from 'src/app/app-nebular.module';
+import { NbDialogService } from '@nebular/theme';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AuditDataComponent', () => {
   let component: AuditDataFormComponent;
   let fixture: ComponentFixture<AuditDataFormComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [AuditDataFormComponent],
-    }).compileComponents();
-  }));
+      imports: [AppNebularModule, BrowserAnimationsModule],
+      providers: [NbDialogService, FormBuilder],
+    });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(AuditDataFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
