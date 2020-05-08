fefa9c31cd1011fbef3dc26b0fe96aff65303336
kexplx <oscar.rosner@web.de>
2020-05-08 02:21:55 +0200

    Add EditAuditDialogComponent

--- ng-frontend/src/app/shared/dialogs/edit-audit-dialog/edit-audit-dialog.component.ts
+++ ng-frontend/src/app/shared/dialogs/edit-audit-dialog/edit-audit-dialog.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { NbDialogRef } from '@nebular/theme';
+import { Store } from '@ngxs/store';
+import { AuditRegistryState } from 'src/app/ngxs/audit-registry.state';
+import { Observable } from 'rxjs';
+import { Audit } from 'src/app/data/models/audit.model';
+import { UpdateAudit } from 'src/app/ngxs/audit.actions';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-edit-audit-dialog',
   templateUrl: './edit-audit-dialog.component.html',
-  styleUrls: ['./edit-audit-dialog.component.scss']
+  styleUrls: ['./edit-audit-dialog.component.scss'],
 })
 export class EditAuditDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    protected dialogRef: NbDialogRef<{ id: string }>,
+    private store: Store,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
   }
 
+  onCancel() {
+    this.dialogRef.close();
+  }
 }
