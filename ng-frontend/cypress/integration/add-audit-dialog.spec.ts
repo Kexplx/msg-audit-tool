9d05f20e27709dfaf1b624694a58bcfe89627953
Cathy Hu <cathy.hu@fau.de>
2020-05-15 15:23:16 +0200

    [TEST] Change e2e test to check if warning displays on cancel

--- ng-frontend/cypress/integration/add-audit-dialog.spec.ts
+++ ng-frontend/cypress/integration/add-audit-dialog.spec.ts
@@ -1,16 +1,22 @@
+import { Audit } from 'src/app/data/models/audit.model';
+
 describe('AddAuditDialog', () => {
   let baseUrl = Cypress.config().baseUrl + '/audits/';
+  let testAudit: Audit;
+
+  before(() => {
+    cy.fixture('example-audit').then(json => {
+      testAudit = json;
+    });
+  });
 
   it('Routing to audits/new opens AddAuditForm', () => {
     cy.visit(baseUrl + 'new');
     cy.get('nb-dialog-container');
   });
 
-  it('Clicking abbrechen button closes form', () => {
+  it('Clicking abbrechen button opens warning message', () => {
     cy.get('.accordion-footer > .status-basic').click();
-
-    cy.get('#cdk-overlay-0 > nb-dialog-container > app-add-audit-dialog > div > form').should(
-      'not.exist',
-    );
+    cy.get('nb-dialog-container > .ng-star-inserted > nb-card > nb-card-header').should('exist');
   });
 });
