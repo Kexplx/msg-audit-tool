2379abc006f83360e2a6709f580c0732dbefd46f
Cathy Hu <cathy.hu@fau.de>
2020-05-15 15:24:49 +0200

    [TEST] Clicking yes on warning message should close overlay

--- ng-frontend/cypress/integration/add-audit-dialog.spec.ts
+++ ng-frontend/cypress/integration/add-audit-dialog.spec.ts
@@ -19,4 +19,11 @@ describe('AddAuditDialog', () => {
     cy.get('.accordion-footer > .status-basic').click();
     cy.get('nb-dialog-container > .ng-star-inserted > nb-card > nb-card-header').should('exist');
   });
+
+  it('Clicking yes on warning message should close overlay', () => {
+    cy.get('.card-footer > :nth-child(2)').click();
+    cy.get('#cdk-overlay-0 > nb-dialog-container > app-add-audit-dialog > div > form').should(
+      'not.exist',
+    );
+  });
 });
