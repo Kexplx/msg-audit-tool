dff9bdaf69a48d142c302bba0ef0489d352aa4f2
Cathy Hu <cathy.hu@fau.de>
2020-05-15 15:27:30 +0200

    [TEST] Add e2e test to validate create audit form function (basic info)

--- ng-frontend/cypress/integration/add-audit-dialog.spec.ts
+++ ng-frontend/cypress/integration/add-audit-dialog.spec.ts
@@ -26,4 +26,25 @@ describe('AddAuditDialog', () => {
       'not.exist',
     );
   });
+
+  it('Form gives inputable element for audit name', () => {
+    cy.visit(baseUrl + 'new');
+    cy.get('.grid-3-1 > :nth-child(1) > .input-full-width').clear().type(testAudit.name);
+    cy.get('.grid-3-1 > :nth-child(1) > .input-full-width').should('have.value', testAudit.name);
+  });
+
+  // it('Form gives choosable element for startdate and enddate', () => {
+  //   cy.get('.ng-tns-c155-1.ng-star-inserted > .ng-trigger > .item-body > .grid-1-1 > :nth-child(1) > .input-full-width').click();
+  //   cy.get('.today > .cell-content').click();
+  //   cy.get('.ng-tns-c155-1.ng-star-inserted > .ng-trigger > .item-body > .grid-1-1 > :nth-child(2) > .input-full-width').click();
+  //   cy.get('.today > .cell-content').click();
+  // });
+
+  it('Clicking on company tab opens accordeon body', () => {
+    cy.get(':nth-child(2) > .accordion-item-header-collapsed').click();
+    cy.get('.ng-tns-c155-3.ng-star-inserted > .ng-trigger > .item-body').should(
+      'contain',
+      'Unternehmen',
+    );
+  });
 });
