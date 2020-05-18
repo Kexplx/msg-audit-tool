0c6ede36a9f20a3c621321d317cf7bcb72b996a5
kexplx <oscar.rosner@web.de>
2020-05-18 04:48:19 +0200

    [26] Move the routing after edit click into link property of nbMenuItem

--- ng-frontend/src/app/audit-list/audit-card/audit-card.component.ts
+++ ng-frontend/src/app/audit-list/audit-card/audit-card.component.ts
@@ -22,28 +22,24 @@ export class AuditCardComponent implements OnInit {
   @Input() audit: Audit;
   nbMenuId: string;
   auditStatus = AuditStatus;
-  items: NbMenuItem[] = [{ title: 'Löschen', icon: 'trash-outline', data: MenuOptions.Delete }];
+  items: NbMenuItem[];
 
   constructor(private nbMenuService: NbMenuService, private store: Store, private router: Router) {}
 
   ngOnInit() {
     const status = this.audit.status;
-    let itemToAdd: NbMenuItem;
-    if (status == AuditStatus.IsPlanned || status == AuditStatus.InAction) {
-      itemToAdd = {
-        title: 'Bearbeiten',
-        icon: 'edit-outline',
-        data: MenuOptions.Edit,
-      };
-    } else {
-      itemToAdd = {
-        title: 'Reaktivieren',
-        icon: 'activity-outline',
-        data: MenuOptions.Reactivate,
-      };
-    }
 
-    this.items = [itemToAdd, ...this.items];
+    this.items = [
+      status == AuditStatus.IsPlanned || status == AuditStatus.InAction
+        ? {
+            title: 'Bearbeiten',
+            icon: 'edit-outline',
+            link: `/audits/${this.audit.id}/edit`,
+            data: MenuOptions.Edit,
+          }
+        : { title: 'Reaktivieren', icon: 'activity-outline', data: MenuOptions.Reactivate },
+      { title: 'Löschen', icon: 'trash-outline', data: MenuOptions.Delete },
+    ];
 
     this.nbMenuId = shortid.generate();
 
@@ -55,10 +51,6 @@ export class AuditCardComponent implements OnInit {
       )
       .subscribe((option: MenuOptions) => {
         switch (option) {
-          case MenuOptions.Edit:
-            const urlTree = this.router.createUrlTree([`/audits/${this.audit.id}/edit`]);
-            this.router.navigateByUrl(urlTree);
-            break;
           case MenuOptions.Delete:
             this.store.dispatch(new DeleteAudit(this.audit));
             break;
