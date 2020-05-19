9bc2b84271f9096cb8c5a1fa07716d5389be0894
Cathy Hu <cathy.hu@fau.de>
2020-05-19 16:03:28 +0200

    [TEST] Create custom command cy.inputAudit

--- ng-frontend/cypress/support/commands.js
+++ ng-frontend/cypress/support/commands.js
@@ -23,3 +23,65 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+function inputAudit(testAudit) {
+  // Input Audit name, start date, end date and open next collapsed accordeon through click
+  cy.get('.grid-3-1 > :nth-child(1) > .input-full-width').clear().type(testAudit.name);
+  if (testAudit.start) {
+    cy.get('.grid-1-1 > :nth-child(1) > .input-full-width').click();
+    cy.get('.today > .cell-content').click();
+  }
+  if (testAudit.end) {
+    cy.get('.grid-1-1 > :nth-child(2) > .input-full-width').click();
+    cy.get('.today > .cell-content').click();
+  }
+  cy.get(':nth-child(2) > .accordion-item-header-collapsed').click();
+
+  // Input Customer data and open next collapsed accordeon through click
+  cy.get(
+    '.ng-tns-c155-3.ng-star-inserted > .ng-trigger > .item-body > :nth-child(1) > :nth-child(1) > .input-full-width',
+  )
+    .clear()
+    .type(testAudit.customerData.name);
+  cy.get(
+    '.ng-tns-c155-3.ng-star-inserted > .ng-trigger > .item-body > :nth-child(1) > :nth-child(2) > .input-full-width',
+  )
+    .clear()
+    .type(testAudit.customerData.department);
+  cy.get(
+    '.ng-tns-c155-3.ng-star-inserted > .ng-trigger > .item-body > :nth-child(2) > :nth-child(1) > .input-full-width',
+  )
+    .clear()
+    .type(testAudit.customerData.corporateDivision);
+  cy.get(
+    '.ng-tns-c155-3.ng-star-inserted > .ng-trigger > .item-body > :nth-child(2) > :nth-child(2) > .input-full-width',
+  )
+    .clear()
+    .type(testAudit.customerData.sector);
+  cy.get(':nth-child(3) > .accordion-item-header-collapsed').click();
+
+  // Input Contact Information and open next collapsed accordeon through click
+  cy.get(':nth-child(1) > .appearance-outline > .select-button').click();
+  cy.get('nb-option').contains(testAudit.contactPerson.title).click();
+  cy.get(
+    '.ng-tns-c155-5.ng-star-inserted > .ng-trigger > .item-body > :nth-child(1) > :nth-child(2) > .input-full-width',
+  )
+    .clear()
+    .type(testAudit.contactPerson.salutation);
+  cy.get(
+    '.ng-tns-c155-5.ng-star-inserted > .ng-trigger > .item-body > :nth-child(2) > :nth-child(1) > .input-full-width',
+  )
+    .clear()
+    .type(testAudit.contactPerson.firstName);
+  cy.get(
+    '.ng-tns-c155-5.ng-star-inserted > .ng-trigger > .item-body > :nth-child(2) > :nth-child(2) > .input-full-width',
+  )
+    .clear()
+    .type(testAudit.contactPerson.lastName);
+  cy.get('.field-item-contact-information > .input-full-width')
+    .clear()
+    .type(testAudit.contactPerson.information);
+  cy.get('.accordion-footer > .status-primary').click();
+}
+
+Cypress.Commands.add('inputAudit', inputAudit);
