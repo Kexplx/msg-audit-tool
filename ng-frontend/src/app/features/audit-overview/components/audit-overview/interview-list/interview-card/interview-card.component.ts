caf3a3a5cfd693cbc91d25b9af8b9e1dd362eee8
kexplx <oscar.rosner@web.de>
2020-05-24 16:51:46 +0200

    [10] Add InterviewCardComponent

---
+++ ng-frontend/src/app/features/audit-overview/components/audit-overview/interview-list/interview-card/interview-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-interview-card',
+  templateUrl: './interview-card.component.html',
+  styleUrls: ['./interview-card.component.scss'],
+})
+export class InterviewCardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
