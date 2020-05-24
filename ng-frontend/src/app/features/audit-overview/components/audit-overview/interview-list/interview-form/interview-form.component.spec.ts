073a20c10342caed74c892439daaf4a25ec34cf5
kexplx <oscar.rosner@web.de>
2020-05-24 16:48:34 +0200

    [10] Add InterviewFormComponent

---
+++ ng-frontend/src/app/features/audit-overview/components/audit-overview/interview-list/interview-form/interview-form.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { InterviewFormComponent } from './interview-form.component';
+
+describe('InterviewFormComponent', () => {
+  let component: InterviewFormComponent;
+  let fixture: ComponentFixture<InterviewFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ InterviewFormComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InterviewFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
