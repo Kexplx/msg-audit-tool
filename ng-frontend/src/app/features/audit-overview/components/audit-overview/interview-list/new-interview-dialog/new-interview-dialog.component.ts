73e77de5133019fa3a824def00f8d7febf02ee4c
kexplx <oscar.rosner@web.de>
2020-05-24 16:47:55 +0200

    [10] Add NewInterviewDialogComponent

---
+++ ng-frontend/src/app/features/audit-overview/components/audit-overview/interview-list/new-interview-dialog/new-interview-dialog.component.ts
@@ -0,0 +1,36 @@
+import { Component, ViewChild, TemplateRef, AfterViewInit } from '@angular/core';
+import { NbDialogService, NbDialogRef } from '@nebular/theme';
+import { Store } from '@ngxs/store';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { defaultDialogOptions } from 'src/app/shared/components/dialogs/default-dialog-options';
+
+@Component({
+  selector: 'app-new-interview-dialog',
+  templateUrl: './new-interview-dialog.component.html',
+  styleUrls: ['./new-interview-dialog.component.scss'],
+})
+export class NewInterviewDialogComponent implements AfterViewInit {
+  @ViewChild('dialog') dialog: TemplateRef<any>;
+  dialogRef: NbDialogRef<any>;
+
+  constructor(private dialogService: NbDialogService, private location: Location) {}
+
+  ngAfterViewInit() {
+    this.dialogRef = this.dialogService.open(this.dialog, {
+      ...defaultDialogOptions,
+    });
+
+    this.dialogRef.onClose.subscribe(() => {
+      this.location.back();
+    });
+  }
+
+  onSubmit() {
+    // this.store.dispatch(new AddAudit(audit)).subscribe(() => this.dialogRef.close());
+  }
+
+  onCancel() {
+    this.dialogRef.close();
+  }
+}
