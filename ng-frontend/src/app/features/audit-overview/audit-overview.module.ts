b76211abacdaa7941e877309f484d01e32c6076d
kexplx <oscar.rosner@web.de>
2020-05-24 16:54:45 +0200

    [10] Update declarations

--- ng-frontend/src/app/features/audit-overview/audit-overview.module.ts
+++ ng-frontend/src/app/features/audit-overview/audit-overview.module.ts
@@ -4,9 +4,20 @@ import { CommonModule } from '@angular/common';
 import { AuditOverviewRoutingModule } from './audit-overview-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AuditOverviewComponent } from './components/audit-overview/audit-overview.component';
+import { InterviewCardComponent } from './components/audit-overview/interview-list/interview-card/interview-card.component';
+import { NewInterviewDialogComponent } from './components/audit-overview/interview-list/new-interview-dialog/new-interview-dialog.component';
+import { InterviewFormComponent } from './components/audit-overview/interview-list/interview-form/interview-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { InterviewListComponent } from './components/audit-overview/interview-list/interview-list.component';
 
 @NgModule({
-  declarations: [AuditOverviewComponent],
-  imports: [CommonModule, AuditOverviewRoutingModule, SharedModule],
+  declarations: [
+    AuditOverviewComponent,
+    InterviewCardComponent,
+    NewInterviewDialogComponent,
+    InterviewFormComponent,
+    InterviewListComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule, AuditOverviewRoutingModule, SharedModule],
 })
 export class AuditOverviewModule {}
