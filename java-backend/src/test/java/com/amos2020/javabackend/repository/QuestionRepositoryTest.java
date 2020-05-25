969f863a77320fe80b88662694f8381b0d10b9b0
Benjamin <beze-FAU>
2020-05-25 13:36:01 +0200

    [38] Update QuestionRepositoryTest

--- java-backend/src/test/java/com/amos2020/javabackend/repository/QuestionRepositoryTest.java
+++ java-backend/src/test/java/com/amos2020/javabackend/repository/QuestionRepositoryTest.java
@@ -1,13 +1,16 @@
 package com.amos2020.javabackend.repository;
 
+import com.amos2020.javabackend.entity.FacCrit;
 import com.amos2020.javabackend.entity.Question;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Example;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.TransactionSystemException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -16,45 +19,65 @@ public class QuestionRepositoryTest {
     @Autowired
     private QuestionRepository repository;
 
+
+    @Autowired
+    FacCritRepository facCritRepository;
+
     private Question question;
+    private FacCrit facCrit;
+
+
+    @Before
+    public void setup() {
+        facCrit = new FacCrit();
+        facCrit.setName("TestFaccrit");
+        facCritRepository.save(facCrit);
+    }
 
     @Test
     public void insertValid() {
         question = new Question();
-        question.setQuestionTextDe("TestFrage?");
+        question.setTextDe("TestFrage?");
+        question.setFaccritId(facCrit.getId());
         repository.save(question);
         Assert.assertTrue(repository.exists(Example.of(question)));
     }
 
-    @Test
+    @Test(expected = TransactionSystemException.class)
     public void insertTextNull() {
         question = new Question();
-        question.setQuestionTextDe(null);
+        question.setTextDe(null);
+        question.setFaccritId(facCrit.getId());
         repository.save(question);
         Assert.assertTrue(repository.exists(Example.of(question)));
     }
+
 
     @Test
     public void changeTextValid() {
         question = new Question();
-        question.setQuestionTextDe("TestFrage?");
+        question.setTextDe("TestFrage?");
+        question.setFaccritId(facCrit.getId());
         Question tmp = repository.save(question);
         Assert.assertTrue(repository.exists(Example.of(question)));
-        tmp.setQuestionTextDe("NeueTestFrage?");
+        tmp.setTextDe("NeueTestFrage?");
         repository.save(tmp);
         Assert.assertTrue(repository.exists(Example.of(tmp)));
-        Assert.assertEquals(tmp.getQuestionTextDe(),question.getQuestionTextDe());
+        Assert.assertEquals(tmp.getTextDe(),question.getTextDe());
     }
 
-    @Test
+    @Test(expected = TransactionSystemException.class)
     public void changeTextNull() {
+
         question = new Question();
-        question.setQuestionTextDe("TestFrage?");
+        question.setTextDe("TestFrage?");
+        question.setFaccritId(facCrit.getId());
         Question tmp = repository.save(question);
         Assert.assertTrue(repository.exists(Example.of(question)));
-        tmp.setQuestionTextDe(null);
+        tmp.setTextDe(null);
         repository.save(tmp);
         Assert.assertTrue(repository.exists(Example.of(tmp)));
-        Assert.assertNull(question.getQuestionTextDe());
+        Assert.assertNull(question.getTextDe());
+
     }
 }
