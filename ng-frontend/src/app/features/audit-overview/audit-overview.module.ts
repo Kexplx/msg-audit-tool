aaa749d71f5b23ec23561936a431ad107d11590d
kexplx <oscar.rosner@web.de>
2020-05-25 11:14:43 +0200

    [10] Add AuditInfoComponent, FilterInterviewsPipe to declarations

--- ng-frontend/src/app/features/audit-overview/audit-overview.module.ts
+++ ng-frontend/src/app/features/audit-overview/audit-overview.module.ts
@@ -9,14 +9,18 @@ import { NewInterviewDialogComponent } from './components/audit-overview/intervi
 import { InterviewFormComponent } from './components/audit-overview/interview-list/interview-form/interview-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { InterviewListComponent } from './components/audit-overview/interview-list/interview-list.component';
+import { FilterInterviewsPipe } from './components/audit-overview/interview-list/filter-interviews.pipe';
+import { AuditInfoComponent } from './components/audit-overview/audit-info/audit-info.component';
 
 @NgModule({
   declarations: [
     AuditOverviewComponent,
     InterviewCardComponent,
+    AuditInfoComponent,
     NewInterviewDialogComponent,
     InterviewFormComponent,
     InterviewListComponent,
+    FilterInterviewsPipe,
   ],
   imports: [CommonModule, ReactiveFormsModule, AuditOverviewRoutingModule, SharedModule],
 })
