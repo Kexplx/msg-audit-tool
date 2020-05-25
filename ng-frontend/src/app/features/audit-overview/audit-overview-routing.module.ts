70812c9243230b0456899fa635ce3437cf1c7676
kexplx <oscar.rosner@web.de>
2020-05-25 10:25:06 +0200

    [10] Add child route for /infos

--- ng-frontend/src/app/features/audit-overview/audit-overview-routing.module.ts
+++ ng-frontend/src/app/features/audit-overview/audit-overview-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuditOverviewComponent } from './components/audit-overview/audit-overview.component';
 import { NewInterviewDialogComponent } from './components/audit-overview/interview-list/new-interview-dialog/new-interview-dialog.component';
 import { InterviewListComponent } from './components/audit-overview/interview-list/interview-list.component';
+import { AuditInfoComponent } from './components/audit-overview/audit-info/audit-info.component';
+import { EditAuditDialogComponent } from 'src/app/shared/components/dialogs/edit-audit-dialog/edit-audit-dialog.component';
 
 const routes: Routes = [
   {
@@ -18,6 +20,11 @@ const routes: Routes = [
         path: 'interviews',
         component: InterviewListComponent,
         children: [{ path: 'new', component: NewInterviewDialogComponent }],
+      },
+      {
+        path: 'infos',
+        component: AuditInfoComponent,
+        children: [{ path: 'edit', component: EditAuditDialogComponent }],
       },
     ],
   },
