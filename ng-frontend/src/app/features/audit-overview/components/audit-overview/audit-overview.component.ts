e3352b769bbfa585f51b22f652c983f1d9d3d411
kexplx <oscar.rosner@web.de>
2020-05-25 10:44:19 +0200

    [10] Add items for nb-tabs containg title, icon, route and responsive flag

--- ng-frontend/src/app/features/audit-overview/components/audit-overview/audit-overview.component.ts
+++ ng-frontend/src/app/features/audit-overview/components/audit-overview/audit-overview.component.ts
@@ -2,15 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Store } from '@ngxs/store';
 import { ActivatedRoute, Router } from '@angular/router';
-import { NbMenuItem } from '@nebular/theme';
 import { tap } from 'rxjs/operators';
-import * as shortid from 'shortid';
 import { Audit } from 'src/app/core/data/models/audit.model';
 import { AuditRegistryState } from 'src/app/core/ngxs/audit-registry.state';
-
-enum MenuOptions {
-  Edit,
-}
 
 @Component({
   selector: 'app-audit-overview',
@@ -19,25 +13,19 @@ enum MenuOptions {
 })
 export class AuditOverviewComponent implements OnInit {
   audit$: Observable<Audit>;
-  items: NbMenuItem[];
-  menuId: string;
   tabs: any[] = [
     {
-      title: 'Users',
-      icon: 'person',
-      route: './tab1',
+      title: 'Interviews',
+      icon: 'attach-2-outline',
+      route: './interviews',
+      responsive: true,
+      active: true,
     },
     {
-      title: 'Orders',
-      icon: 'paper-plane-outline',
+      title: 'infos',
+      icon: 'info-outline',
+      route: './infos',
       responsive: true,
-      route: ['./tab2'],
-    },
-    {
-      title: 'Transaction',
-      icon: 'flash-outline',
-      responsive: true,
-      disabled: true,
     },
   ];
 
@@ -61,15 +49,5 @@ export class AuditOverviewComponent implements OnInit {
           this.router.navigate(['/audits']);
         },
       );
-
-    this.menuId = shortid.generate();
-    this.items = [
-      {
-        title: 'Bearbeiten',
-        icon: 'edit-outline',
-        data: MenuOptions.Edit,
-        link: `audits/${id}/overview/edit`,
-      },
-    ];
   }
 }
