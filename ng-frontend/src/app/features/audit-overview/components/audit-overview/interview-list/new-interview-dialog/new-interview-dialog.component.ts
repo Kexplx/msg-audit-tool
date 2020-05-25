e1e952f3ee5d26cc3858adc3bebd48f8a9bab573
kexplx <oscar.rosner@web.de>
2020-05-25 11:05:49 +0200

    [10] Implement OnInit

--- ng-frontend/src/app/features/audit-overview/components/audit-overview/interview-list/new-interview-dialog/new-interview-dialog.component.ts
+++ ng-frontend/src/app/features/audit-overview/components/audit-overview/interview-list/new-interview-dialog/new-interview-dialog.component.ts
@@ -1,19 +1,37 @@
-import { Component, ViewChild, TemplateRef, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, TemplateRef, AfterViewInit, OnInit } from '@angular/core';
 import { NbDialogService, NbDialogRef } from '@nebular/theme';
 import { Store } from '@ngxs/store';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { defaultDialogOptions } from 'src/app/shared/components/dialogs/default-dialog-options';
+import { AuditRegistryState } from 'src/app/core/ngxs/audit-registry.state';
+import { Observable } from 'rxjs';
+import { Audit } from 'src/app/core/data/models/audit.model';
+import { Interview } from 'src/app/core/data/models/interview.model';
+import { AddInterview } from 'src/app/core/ngxs/audit.actions';
 
 @Component({
   selector: 'app-new-interview-dialog',
   templateUrl: './new-interview-dialog.component.html',
   styleUrls: ['./new-interview-dialog.component.scss'],
 })
-export class NewInterviewDialogComponent implements AfterViewInit {
+export class NewInterviewDialogComponent implements AfterViewInit, OnInit {
   @ViewChild('dialog') dialog: TemplateRef<any>;
   dialogRef: NbDialogRef<any>;
 
+  audit$: Observable<Audit>;
+
+  constructor(
+    private dialogService: NbDialogService,
+    private store: Store,
+    private router: Router,
+    private location: Location,
+  ) {}
+
+  /**
+   * Parses the audit id from router.url and selects the
+   * audit as an observable from the store
+   */
   ngOnInit() {
     const idRegex = /\/audits\/([^\/]*)\/.*/gm;
     const id = idRegex.exec(this.router.url)[1];
