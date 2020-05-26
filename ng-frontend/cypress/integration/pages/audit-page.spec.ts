2570f59a0630efa9d72c8a8f0f8d73035d82ed29
Cathy Hu <cathy.hu@fau.de>
2020-05-26 22:47:48 +0200

    [TEST] Fix and refactor e2e audit page test

--- ng-frontend/cypress/integration/pages/audit-page.spec.ts
+++ ng-frontend/cypress/integration/pages/audit-page.spec.ts
@@ -1,4 +1,4 @@
-import { factors } from '../../src/app/core/data/factors';
+import { factors } from '../../../src/app/core/data/factors';
 
 describe('AuditPage', () => {
   let baseUrl = Cypress.config().baseUrl;
@@ -6,41 +6,75 @@ describe('AuditPage', () => {
   let testAuditUrl;
 
   before(() => {
-    cy.visit(baseUrl);
-    cy.fixture('audits/example-audit').then(json => {
-      testAudit = json;
+    cy.fixture('audits/example-audit')
+      .then(json => {
+        testAudit = json;
+      })
+      .then(() => {
+        cy.visit(baseUrl);
+        cy.get('[data-cy=new-audit]').click();
+        cy.inputAudit(testAudit);
+        cy.get('[data-cy=audit-short-infos]').first().click();
+        cy.url().then(url => {
+          testAuditUrl = url;
+        });
+      });
+  });
+
+  beforeEach(() => {
+    cy.server();
+    cy.route(testAuditUrl);
+  });
+
+  it('generates a valid audit page with a unique id after adding an audit', () => {
+    cy.url().should('contain', 'interviews');
+  });
+
+  context('When in interview overview it ...', () => {
+    before(() => {
+      cy.server();
+      cy.route(testAuditUrl);
+    });
+
+    it('displays an overview with all chosen factors', () => {
+      for (let i = 0; i < factors.length; i++) {
+        let factor_obj = `:nth-child(${i + 1}) > nb-card-header`;
+        cy.get(factor_obj).should('contain.text', factors[i].title);
+      }
+    });
+
+    it('displays a button to new interviews', () => {
+      cy.get('[data-cy=new-interview]').click();
+      cy.get('[data-cy=add-interview-form]').should('exist');
+      cy.get('[data-cy=cancel-interview-data-form]').click();
+    });
+
+    // it('Should display an overview with all chosen categories', () => {
+    //   cy.get(':nth-child(1) > .tab-link').click();
+    //   for (let i = 0; i < factors.length; i++) {
+    //     const categories = factors[i].categories;
+    //     cy.get(`:nth-child(${i + 1}) > nb-list > nb-list-item`).should(
+    //       'contain.text',
+    //       categories[0].title,
+    //     );
+    //   }
+    // });
+  });
+
+  context('When on info page it ...', () => {
+    before(() => {
+      cy.server();
+      cy.route(testAuditUrl);
+      cy.contains('info').click();
+    });
+
+    it('generates a valid audit info page displaying audit information', () => {
+      cy.testAuditInfoPage(testAudit);
+    });
+
+    it('shows a button to edit the audit information', () => {
+      cy.get('[data-cy=audit-options]').click();
+      cy.url().should('contain', 'edit');
     });
   });
-
-  it('Should generate a valid test page with a unique id after adding an audit', () => {
-    cy.get('[data-cy=new-audit]').click();
-    cy.inputAudit(testAudit);
-    cy.get('[data-cy=audit-short-infos]').first().click();
-    cy.url().should('contain', 'overview');
-    testAuditUrl = cy.url();
-  });
-
-  it('Should generate a valid info page displaying audit information', () => {
-    cy.contains('Info').click();
-    cy.testAuditInfoPage(testAudit);
-  });
-
-  // it('Should display an overview with all chosen factors', () => {
-  //   cy.get(':nth-child(1) > .tab-link').click();
-  //   for (let i = 0; i < factors.length; i++) {
-  //     let factor_obj = `:nth-child(${i + 1}) > nb-card-header`;
-  //     cy.get(factor_obj).should('contain.text', factors[i].title);
-  //   }
-  // });
-
-  // it('Should display an overview with all chosen categories', () => {
-  //   cy.get(':nth-child(1) > .tab-link').click();
-  //   for (let i = 0; i < factors.length; i++) {
-  //     const categories = factors[i].categories;
-  //     cy.get(`:nth-child(${i + 1}) > nb-list > nb-list-item`).should(
-  //       'contain.text',
-  //       categories[0].title,
-  //     );
-  //   }
-  // });
 });
