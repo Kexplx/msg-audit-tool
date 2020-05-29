73744ef394ff986ab7bec1bd5e908b280b33b2bb
Cathy Hu <cathy.hu@fau.de>
2020-05-29 16:26:53 +0200

    [TEST] Fix letter case in navbar and sidebar test

--- ng-frontend/cypress/integration/shared/navbar.spec.ts
+++ ng-frontend/cypress/integration/shared/navbar.spec.ts
@@ -3,19 +3,19 @@ describe('Navbar', () => {
     cy.visit(Cypress.config().baseUrl);
   });
 
-  it('Contains Menu Icon Button', () => {
+  it('contains Menu Icon Button', () => {
     cy.get('[data-cy=toggle-sidebar]');
   });
 
-  it('Contains "Audit"', () => {
+  it('contains "Audit"', () => {
     cy.get('[data-cy=home]');
   });
 
-  it('Contains GitHub Icon Button', () => {
+  it('contains GitHub Icon Button', () => {
     cy.get('[data-cy=github]');
   });
 
-  it('Clicking the sidebar toggle button opens the sidebar', () => {
+  it('opens the sidebar when clicking the sidebar toggle button', () => {
     cy.viewport('iphone-5');
     cy.get('[data-cy=toggle-sidebar]').click();
     cy.get('[data-cy=sidebar]').should('have.class', 'expanded');
--- ng-frontend/cypress/integration/shared/sidebar.spec.ts
+++ ng-frontend/cypress/integration/shared/sidebar.spec.ts
@@ -3,16 +3,16 @@ describe('AppNavbarComponent', () => {
     cy.visit(Cypress.config().baseUrl);
   });
 
-  it('Contains "Sidebar" in sidebar content', () => {
+  it('contains "Sidebar" in sidebar content', () => {
     cy.get('[data-cy=sidebar-content]').contains('Sidebar');
   });
 
-  it('Is hidden on initial load on mobile', () => {
+  it('is hidden on initial load on mobile', () => {
     cy.viewport('iphone-5');
     cy.get('[data-cy=sidebar]').should('have.class', 'collapsed');
   });
 
-  it('Is fixed on mobile', () => {
+  it('is fixed on mobile', () => {
     cy.viewport('iphone-5');
     cy.get('[data-cy=sidebar]').should('have.class', 'fixed');
   });
