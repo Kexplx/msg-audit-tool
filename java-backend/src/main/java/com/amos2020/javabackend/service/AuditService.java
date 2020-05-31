66019d257e64045365919ac914bc5dc264aaf8b9
beze-FAU <benjamin.zenke@fau.de>
2020-05-31 20:29:53 +0200

    [29] Add CancelAuditRequest, adapt AuditService and AuditController

--- java-backend/src/main/java/com/amos2020/javabackend/service/AuditService.java
+++ java-backend/src/main/java/com/amos2020/javabackend/service/AuditService.java
@@ -3,8 +3,6 @@ package com.amos2020.javabackend.service;
 import com.amos2020.javabackend.entity.Audit;
 import com.amos2020.javabackend.entity.AuditStatus;
 import com.amos2020.javabackend.repository.AuditRepository;
-import javassist.NotFoundException;
-import net.bytebuddy.implementation.bind.MethodDelegationBinder;
 import org.springframework.stereotype.Service;
 
 import java.sql.Date;
@@ -30,19 +28,8 @@ public class AuditService {
         if (endDate != null) {
             audit.setEndDate(endDate);
         }
-        return repository.save(audit);
-    }
-
-    public Audit getAuditById(int auditId) throws NotFoundException {
-        Optional<Audit> audit = repository.findById(auditId);
-        if (!audit.isPresent()) {
-            throw new NotFoundException("No audit found with id " + audit);
-        }
-        return audit.get();
-    }
-
-    public Audit updateAudit(Audit audit) {
-        return repository.save(audit);
+        repository.save(audit);
+        return audit;
     }
 
 
