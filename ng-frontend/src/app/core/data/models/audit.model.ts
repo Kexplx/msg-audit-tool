f00565e8b8fe3482593254b3bf913addfccfc74e
Oscar Rosner <oscar.rosner@web.de>
2020-06-01 05:49:11 +0200

    [REFACTOR] Sync with db-schema
    Fix typo in AuditStatus
    contactPeople (m to m relation)
    facCrits instead of factors with criterias

--- ng-frontend/src/app/core/data/models/audit.model.ts
+++ ng-frontend/src/app/core/data/models/audit.model.ts
@@ -1,24 +1,22 @@
-import { CustomerData } from './customer-data.model';
 import { ContactPerson } from './contact-person.model';
-import { Factor } from './factor.model';
 import { Interview } from './interview.model';
+import { FacCrit } from './faccrit.model';
 
 export enum AuditStatus {
-  IsPlanned,
+  Planned,
   InAction,
-  IsFinished,
-  IsCanceled,
+  Finished,
+  Cancelled,
 }
 
 export interface Audit {
   id?: string;
-  name: string;
-  start?: number;
-  end?: number;
-  customerData?: CustomerData;
-  contactPerson?: ContactPerson;
-  scope?: Factor[];
   status: AuditStatus;
   creationDate: number;
+  name: string;
+  start: number;
+  end?: number;
+  contactPeople?: ContactPerson[];
   interviews?: Interview[];
+  facCrits: FacCrit[];
 }
