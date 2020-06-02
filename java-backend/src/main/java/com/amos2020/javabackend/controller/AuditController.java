dc137d6aa0db31aef8db6325e3f6993b0686d79d
beze-FAU <benjamin.zenke@fau.de>
2020-06-02 14:30:20 +0200

    [49] Add GET-Endpoint for fetching a list of all audits

--- java-backend/src/main/java/com/amos2020/javabackend/controller/AuditController.java
+++ java-backend/src/main/java/com/amos2020/javabackend/controller/AuditController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @RestController
@@ -143,6 +144,27 @@ public class AuditController {
             return ResponseEntity.notFound().build();
         } catch (IllegalAccessException e) {
             return ResponseEntity.status(401).build();
+        }
+        return ResponseEntity.ok(response);
+    }
+
+
+    /**
+     * GET endpoint for fetching a list of all audits
+     *
+     * @return List<BasicAuditResponse>
+     */
+    @GetMapping("/audit/all")
+    public ResponseEntity<List<BasicAuditResponse>> getAuditAll() {
+        List<BasicAuditResponse> response = new ArrayList<BasicAuditResponse>();
+        try {
+            for(Audit a : auditService.getAll()) {
+
+                response.add(buildBasicResponse(a));
+            }
+
+        } catch (NotFoundException e) {
+            return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(response);
     }
