72fa7b1a311571ad3c59eb798939327e9dba5303
Oscar Rosner <oscar.rosner@web.de>
2020-06-04 09:03:40 +0200

    [73] Declare ContactPersonCard component

--- ng-frontend/src/app/features/contact-people/contact-people.module.ts
+++ ng-frontend/src/app/features/contact-people/contact-people.module.ts
@@ -5,9 +5,10 @@ import { ContactPeopleRoutingModule } from './contact-people-routing.module';
 import { ContactPeopleListComponent } from './components/contact-people-list/contact-people-list.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CoreModule } from 'src/app/core/core.module';
+import { ContactPersonCardComponent } from './components/contact-people-list/contact-person-card/contact-person-card.component';
 
 @NgModule({
-  declarations: [ContactPeopleListComponent],
+  declarations: [ContactPeopleListComponent, ContactPersonCardComponent],
   imports: [CommonModule, ContactPeopleRoutingModule, SharedModule],
 })
 export class ContactPeopleModule {}
