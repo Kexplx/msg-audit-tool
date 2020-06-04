fbdaf0be07a2908facbfd675ba139d4f2334c9e5
Oscar Rosner <oscar.rosner@web.de>
2020-06-04 08:44:15 +0200

    [73] Select contactPeople slice from store

--- ng-frontend/src/app/features/contact-people/components/contact-people-list/contact-people-list.component.ts
+++ ng-frontend/src/app/features/contact-people/components/contact-people-list/contact-people-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Select } from '@ngxs/store';
+import { AuditRegistryState } from 'src/app/core/ngxs/audit-registry.state';
+import { Observable } from 'rxjs';
+import { ContactPerson } from 'src/app/core/data/models/contact-person.model';
 
 @Component({
   selector: 'app-contact-people-list',
@@ -6,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-people-list.component.scss'],
 })
 export class ContactPeopleListComponent implements OnInit {
-  constructor() {}
+  @Select(AuditRegistryState.contactPeople) contactPeople$: Observable<ContactPerson[]>;
 
   ngOnInit() {}
 }
