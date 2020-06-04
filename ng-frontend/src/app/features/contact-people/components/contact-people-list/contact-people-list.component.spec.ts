4eb9042fb45027bc524313f09fc54d96cbc87935
Oscar Rosner <oscar.rosner@web.de>
2020-06-04 07:52:15 +0200

    [73] Add ContactPeopleList

---
+++ ng-frontend/src/app/features/contact-people/components/contact-people-list/contact-people-list.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { ContactPeopleListComponent } from './contact-people-list.component';
+
+describe('ContactPeopleListComponent', () => {
+  let component: ContactPeopleListComponent;
+  let fixture: ComponentFixture<ContactPeopleListComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ContactPeopleListComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ContactPeopleListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
