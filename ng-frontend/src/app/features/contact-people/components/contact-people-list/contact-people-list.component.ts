348bad63e5f2a25d4f045efc5d0e9705a532258a
Oscar Rosner <oscar.rosner@web.de>
2020-06-04 07:52:15 +0200

    [73] Add ContactPeopleList

---
+++ ng-frontend/src/app/features/contact-people/components/contact-people-list/contact-people-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-people-list',
+  templateUrl: './contact-people-list.component.html',
+  styleUrls: ['./contact-people-list.component.scss'],
+})
+export class ContactPeopleListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
