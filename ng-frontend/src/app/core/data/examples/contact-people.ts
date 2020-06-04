57951fecc06d4c10107215e3058751b0f4f84447
Oscar Rosner <oscar.rosner@web.de>
2020-06-04 07:38:34 +0200

    [73] Add samples for contact people

---
+++ ng-frontend/src/app/core/data/examples/contact-people.ts
@@ -0,0 +1,30 @@
+import { ContactPerson } from '../models/contact-person.model';
+
+export const contactPeople: ContactPerson[] = [
+  {
+    companyName: 'BMW',
+    contactInformation: 'Contact Information 123',
+    firstName: 'Oscar',
+    lastName: 'Rosner',
+    role: 'Software Developer',
+    salutation: 'Herr',
+    title: 'Dr. ',
+    corporateDivision: 'BMW Robotics',
+    department: 'IT-Abteilung',
+    id: '7891209sdkmlds9',
+    sector: 'Automobilindustrie',
+  },
+  {
+    companyName: 'BMW',
+    contactInformation: 'Contact Information 123',
+    firstName: 'Cathy',
+    lastName: 'Wu',
+    role: 'Software Developer',
+    salutation: 'Frau',
+    title: 'Dr. ',
+    corporateDivision: 'BMW Robotics',
+    department: 'IT-Abteilung',
+    id: '7891209sdkmlds9',
+    sector: 'Automobilindustrie',
+  },
+];
