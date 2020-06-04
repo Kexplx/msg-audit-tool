7624cbf609c542399c64cbebbbc67bee5b7ebae7
Oscar Rosner <oscar.rosner@web.de>
2020-06-04 07:41:58 +0200

    [73] Change nullable properties

--- ng-frontend/src/app/core/data/models/contact-person.model.ts
+++ ng-frontend/src/app/core/data/models/contact-person.model.ts
@@ -4,10 +4,10 @@ export interface ContactPerson {
   title: string;
   firstName: string;
   lastName: string;
-  contactInformation: string;
+  companyName: string;
   role: string;
-  sector?: string;
-  name?: string;
-  department?: string;
-  corporateDivision?: string;
+  sector: string;
+  department: string;
+  corporateDivision: string;
+  contactInformation?: string;
 }
