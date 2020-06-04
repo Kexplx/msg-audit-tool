638e168d1d35d0279b101769c8852d79681b1eb1
Oscar Rosner <oscar.rosner@web.de>
2020-06-04 12:09:57 +0200

    [73] Add sample audits

--- ng-frontend/src/app/core/data/examples/audits.ts
+++ ng-frontend/src/app/core/data/examples/audits.ts
@@ -20,4 +20,22 @@ export const audits: Audit[] = [
     status: AuditStatus.Finished,
     contactPeople,
   },
+  {
+    id: '1s2sgdsdgffgfd3',
+    creationDate: Date.now(),
+    name: 'Audit 2',
+    startDate: Date.now(),
+    facCrits: [],
+    status: AuditStatus.Finished,
+    contactPeople,
+  },
+  {
+    id: '12sgdfggsdffd3',
+    creationDate: Date.now(),
+    name: 'Audit 2',
+    startDate: Date.now(),
+    facCrits: [],
+    status: AuditStatus.Finished,
+    contactPeople,
+  },
 ];
