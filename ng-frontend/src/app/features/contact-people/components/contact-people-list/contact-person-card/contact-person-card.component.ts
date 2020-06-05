a8f4c8dd61a4d93afbb73eca9f7953c43a029dfd
Oscar Rosner <oscar.rosner@web.de>
2020-06-05 17:46:11 +0200

    [73] Dispatch DeleteContactPerson action onDelete

--- ng-frontend/src/app/features/contact-people/components/contact-people-list/contact-person-card/contact-person-card.component.ts
+++ ng-frontend/src/app/features/contact-people/components/contact-people-list/contact-person-card/contact-person-card.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ContactPerson } from 'src/app/core/data/models/contact-person.model';
+import { Store } from '@ngxs/store';
+import { DeleteContactPerson } from 'src/app/core/ngxs/audit.actions';
 
 @Component({
   selector: 'app-contact-person-card',
   templateUrl: './contact-person-card.component.html',
   styleUrls: ['./contact-person-card.component.scss'],
 })
-export class ContactPersonCardComponent implements OnInit {
+export class ContactPersonCardComponent {
   @Input() contactPerson: ContactPerson;
 
-  constructor() {}
+  constructor(private store: Store) {}
 
-  ngOnInit() {}
+  onDelete() {
+    this.store.dispatch(new DeleteContactPerson(this.contactPerson));
+  }
 }
