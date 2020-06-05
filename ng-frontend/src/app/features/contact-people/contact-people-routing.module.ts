31e9018dbdac939833ea95dc2c1d4d0375816f38
Oscar Rosner <oscar.rosner@web.de>
2020-06-05 17:44:48 +0200

    [73] Add child route for id/edit

--- ng-frontend/src/app/features/contact-people/contact-people-routing.module.ts
+++ ng-frontend/src/app/features/contact-people/contact-people-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContactPeopleListComponent } from './components/contact-people-list/contact-people-list.component';
 import { AddContactPersonDialogComponent } from 'src/app/shared/components/dialogs/add-contact-person-dialog/add-contact-person-dialog.component';
+import { EditContactPersonDialogComponent } from 'src/app/shared/components/dialogs/edit-contact-person-dialog/edit-contact-person-dialog.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
       {
         path: 'new',
         component: AddContactPersonDialogComponent,
+      },
+      {
+        path: ':id/edit',
+        component: EditContactPersonDialogComponent,
       },
     ],
   },
