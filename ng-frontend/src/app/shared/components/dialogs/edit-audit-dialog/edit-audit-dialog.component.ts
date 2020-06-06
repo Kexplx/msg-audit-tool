80d14bbdba68caba77fc51677f24ebd50a68b9bc
Oscar Rosner <oscar.rosner@web.de>
2020-06-06 17:49:27 +0200

    [73] Select contact-people from store

--- ng-frontend/src/app/shared/components/dialogs/edit-audit-dialog/edit-audit-dialog.component.ts
+++ ng-frontend/src/app/shared/components/dialogs/edit-audit-dialog/edit-audit-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, TemplateRef, ViewChild, AfterViewInit } from '@angular/core';
 import { NbDialogRef, NbDialogService } from '@nebular/theme';
-import { Store } from '@ngxs/store';
+import { Store, Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
@@ -9,6 +9,7 @@ import { defaultDialogOptions } from '../default-dialog-options';
 import { Audit } from 'src/app/core/data/models/audit.model';
 import { AuditRegistryState } from 'src/app/core/ngxs/audit-registry.state';
 import { UpdateAudit } from 'src/app/core/ngxs/audit.actions';
+import { ContactPerson } from 'src/app/core/data/models/contact-person.model';
 
 @Component({
   selector: 'app-edit-audit-dialog',
@@ -17,6 +18,7 @@ import { UpdateAudit } from 'src/app/core/ngxs/audit.actions';
 })
 export class EditAuditDialogComponent implements OnInit, AfterViewInit {
   @ViewChild('dialog') dialog: TemplateRef<any>;
+  @Select(AuditRegistryState.contactPeople) contactPeople$: Observable<ContactPerson[]>;
   dialogRef: NbDialogRef<any>;
 
   audit$: Observable<Audit>;
