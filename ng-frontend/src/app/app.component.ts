c44b1ddd21ba2fa70caa8482e84ef5466e04198d
Oscar Rosner <oscar.rosner@web.de>
2020-06-06 17:45:12 +0200

    [FIX] Select audits and contact people

--- ng-frontend/src/app/app.component.ts
+++ ng-frontend/src/app/app.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { NbSidebarService } from '@nebular/theme';
+import { Store, Select } from '@ngxs/store';
+import { AuditRegistryState } from './core/ngxs/audit-registry.state';
+import { Observable } from 'rxjs';
+import { Audit } from './core/data/models/audit.model';
+import { ContactPerson } from './core/data/models/contact-person.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  @Select(AuditRegistryState.audits) audits$: Observable<Audit[]>;
+  @Select(AuditRegistryState.contactPeople) contactPeople$: Observable<ContactPerson[]>;
+
+  constructor(private sidebarService: NbSidebarService, private store: Store) {}
+
+  toggleSidebar() {
+    this.sidebarService.toggle();
+  }
+}
