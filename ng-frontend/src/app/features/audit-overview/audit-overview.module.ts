b09285935b919ed3c1daadb19b846186a38ff12c
Oscar Rosner <oscar.rosner@web.de>
2020-06-07 19:31:19 +0200

    [73] Remove FilterInterview pipe

--- ng-frontend/src/app/features/audit-overview/audit-overview.module.ts
+++ ng-frontend/src/app/features/audit-overview/audit-overview.module.ts
@@ -9,7 +9,6 @@ import { NewInterviewDialogComponent } from './components/audit-overview/intervi
 import { InterviewFormComponent } from './components/audit-overview/interview-list/interview-form/interview-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { InterviewListComponent } from './components/audit-overview/interview-list/interview-list.component';
-import { FilterInterviewsPipe } from './components/audit-overview/interview-list/filter-interviews.pipe';
 import { AuditInfoComponent } from './components/audit-overview/audit-info/audit-info.component';
 
 @NgModule({
@@ -20,7 +19,6 @@ import { AuditInfoComponent } from './components/audit-overview/audit-info/audit
     NewInterviewDialogComponent,
     InterviewFormComponent,
     InterviewListComponent,
-    FilterInterviewsPipe,
   ],
   imports: [CommonModule, ReactiveFormsModule, AuditOverviewRoutingModule, SharedModule],
 })
