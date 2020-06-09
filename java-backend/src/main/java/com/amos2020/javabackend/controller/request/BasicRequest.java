d6428a677f4ab03bcb9c9f10e8cd6d0da0bf41c3
Markus Goller <markus.goller@fau.de>
2020-06-09 14:45:20 +0200

    [REFACTOR] Add missing tags for comments

--- java-backend/src/main/java/com/amos2020/javabackend/controller/request/BasicRequest.java
+++ java-backend/src/main/java/com/amos2020/javabackend/controller/request/BasicRequest.java
@@ -15,7 +15,7 @@ public abstract class BasicRequest {
     /**
      * Asserts that an attribute that represents a name cannot be null, empty or longer than 45 chars
      *
-     * @param name
+     * @param name String
      */
     public void assertNameIsValid(String name) {
         if (name == null || StringUtils.isBlank(name)) {
@@ -29,7 +29,7 @@ public abstract class BasicRequest {
     /**
      * Asserts that an attribute that represent a note cannot be null, empty or longer than 256 chars
      *
-     * @param note
+     * @param note String
      */
     public void assertNoteIsValid(String note){
         if (note == null || StringUtils.isBlank(note)) {
@@ -43,7 +43,7 @@ public abstract class BasicRequest {
     /**
      * Asserts that a date cannot be null
      *
-     * @param date
+     * @param date String
      */
     public void assertDateIsNotNull(Date date) {
         if (date == null) {
@@ -54,8 +54,8 @@ public abstract class BasicRequest {
     /**
      * Asserts that end date cannot be older than start date
      *
-     * @param startDate
-     * @param endDate
+     * @param startDate Date
+     * @param endDate Date
      */
     public void assertDatesAreValid(Date startDate, Date endDate) {
         if (endDate!= null && endDate.before(startDate)) {
@@ -66,7 +66,7 @@ public abstract class BasicRequest {
     /**
      * Asserts that all ids in a list are valid
      *
-     * @param ids
+     * @param ids List<Integer>
      */
     public void assertIdsAreValid(List<Integer> ids) {
         if(ids == null){
@@ -81,7 +81,7 @@ public abstract class BasicRequest {
     /**
      * Asserts that the given id is valid
      *
-     * @param id
+     * @param id Integer
      */
     public void assertIdIsValid(Integer id) {
         if (id == null || id < 1) {
