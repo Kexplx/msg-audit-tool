11b8bc471140d59d66c8fbe3c77bfe1d7a7f3577
Dennis Possart <dennis.possart@gmail.com>
2020-06-09 19:46:34 +0200

    [62] add and implement BasicAnswerResponse

--- java-backend/src/main/java/com/amos2020/javabackend/controller/response/BasicAnswerResponse.java
+++ java-backend/src/main/java/com/amos2020/javabackend/controller/response/BasicAnswerResponse.java
@@ -1,4 +1,57 @@
 package com.amos2020.javabackend.controller.response;
 
+import com.amos2020.javabackend.entity.Answer;
+import lombok.Getter;
+import lombok.Setter;
+
 public class BasicAnswerResponse {
+
+    public BasicAnswerResponse(Answer answer){
+        this.questionId = answer.getQuestionId();
+        this.interviewId = answer.getInterviewId();
+        this.result = answer.getResult();
+        this.responsible = answer.getResponsible();
+        this.documentation = answer.getDocumentation();
+        this.procedure = answer.getProcedure();
+        this.reason = answer.getReason();
+        this.proof= answer.getProof();
+        this.annotation = answer.getAnnotation();
+    }
+
+    @Getter
+    @Setter
+    private int questionId;
+
+    @Getter
+    @Setter
+    private int interviewId;
+
+    @Getter
+    @Setter
+    private String annotation;
+
+    @Getter
+    @Setter
+    private String proof;
+
+    @Getter
+    @Setter
+    private String reason;
+
+    @Getter
+    @Setter
+    private Boolean procedure;
+
+    @Getter
+    @Setter
+    private Boolean documentation;
+
+    @Getter
+    @Setter
+    private Boolean responsible;
+
+    @Getter
+    @Setter
+    private Boolean result;
+
 }
