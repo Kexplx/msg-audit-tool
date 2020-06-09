3b37a2661e649e9b41b43f4ecf3b6ab51799d345
Markus Goller <markus.goller@fau.de>
2020-06-09 14:41:41 +0200

    [56] Add InterviewService with method for receiving a specific interview from the database

---
+++ java-backend/src/main/java/com/amos2020/javabackend/service/InterviewService.java
@@ -0,0 +1,33 @@
+package com.amos2020.javabackend.service;
+
+import com.amos2020.javabackend.entity.Interview;
+import com.amos2020.javabackend.repository.InterviewRepository;
+import javassist.NotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class InterviewService {
+    final InterviewRepository repository;
+
+
+    public InterviewService(InterviewRepository repository) {
+        this.repository = repository;
+    }
+
+    /**
+     * Returns a interview for a specific id or throws NotFoundException
+     *
+     * @param interviewId int
+     * @return Interview
+     * @throws NotFoundException exception
+     */
+    public Interview getInterviewById(int interviewId) throws NotFoundException {
+        Optional<Interview> interview = repository.findById(interviewId);
+        if (!interview.isPresent()) {
+            throw new NotFoundException("No interview found with id " + interview);
+        }
+        return interview.get();
+    }
+}
