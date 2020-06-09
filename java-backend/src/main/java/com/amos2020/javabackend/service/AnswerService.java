b499f78990f87b6d95914f8e377177c3c04a64dc
Dennis Possart <dennis.possart@gmail.com>
2020-06-09 16:33:20 +0200

    [62] implement AnswerService constructor

--- java-backend/src/main/java/com/amos2020/javabackend/service/AnswerService.java
+++ java-backend/src/main/java/com/amos2020/javabackend/service/AnswerService.java
@@ -1,4 +1,12 @@
 package com.amos2020.javabackend.service;
 
+import com.amos2020.javabackend.repository.AnswerRepository;
+
 public class AnswerService {
+
+    private final AnswerRepository repository;
+
+    public AnswerService(AnswerRepository answerRepository) {
+        this.repository = answerRepository;
+    }
 }
