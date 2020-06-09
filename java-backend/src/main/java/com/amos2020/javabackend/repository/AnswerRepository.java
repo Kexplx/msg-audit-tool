d299ee0ef7d1c89541c2def8c81dffd6aa1d3094
Dennis Possart <dennis.possart@gmail.com>
2020-06-09 16:57:53 +0200

    [62] remove unused import

--- java-backend/src/main/java/com/amos2020/javabackend/repository/AnswerRepository.java
+++ java-backend/src/main/java/com/amos2020/javabackend/repository/AnswerRepository.java
@@ -1,7 +1,6 @@
 package com.amos2020.javabackend.repository;
 
 import com.amos2020.javabackend.entity.Answer;
-import com.amos2020.javabackend.entity.Scope;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnswerRepository extends JpaRepository<Answer, Integer> {
