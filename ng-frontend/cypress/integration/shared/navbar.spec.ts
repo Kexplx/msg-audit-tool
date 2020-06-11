c7e98018967857fd7cb81ab5ecefdc17196011c3
Cathy Hu <cathy.hu@fau.de>
2020-06-11 17:29:46 +0200

    [99] Fix tests for navbar

--- ng-frontend/cypress/integration/shared/navbar.spec.ts
+++ ng-frontend/cypress/integration/shared/navbar.spec.ts
@@ -1,23 +1,25 @@
-// describe('Navbar', () => {
-//   beforeEach(() => {
-//     cy.visit(Cypress.config().baseUrl);
-//   });
+describe('Navbar', () => {
+  beforeEach(() => {
+    cy.visit(Cypress.config().baseUrl);
+  });
 
-//   it('contains Menu Icon Button', () => {
-//     cy.get('[data-cy=toggle-sidebar]');
-//   });
+  it('contains Menu Icon Button', () => {
+    cy.get('[data-cy=toggle-sidebar]');
+  });
 
-//   it('contains "Audit"', () => {
-//     cy.get('[data-cy=home]');
-//   });
+  it('contains home button', () => {
+    cy.get('[data-cy=home]').click();
+    cy.url().should('contain', 'audits');
+  });
 
-//   it('contains GitHub Icon Button', () => {
-//     cy.get('[data-cy=github]');
-//   });
+  it('contains contacts button', () => {
+    cy.get('[data-cy=contacts]').click();
+    cy.url().should('contain', 'contact-people');
+  });
 
-//   it('opens the sidebar when clicking the sidebar toggle button', () => {
-//     cy.viewport('iphone-5');
-//     cy.get('[data-cy=toggle-sidebar]').click();
-//     cy.get('[data-cy=sidebar]').should('have.class', 'expanded');
-//   });
-// });
+  it('opens the sidebar when clicking the sidebar toggle button', () => {
+    cy.viewport('iphone-5');
+    cy.get('[data-cy=toggle-sidebar]').click();
+    cy.get('[data-cy=sidebar]').should('have.class', 'expanded');
+  });
+});
