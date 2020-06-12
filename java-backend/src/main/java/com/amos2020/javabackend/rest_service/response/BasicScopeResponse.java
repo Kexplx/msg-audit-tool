b42e346ee52067303899223baa8c451a7eb4b730
Dennis Possart <dennis.possart@gmail.com>
2020-06-12 12:03:00 +0200

    [78] implement BasicScopeResponse

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/response/BasicScopeResponse.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/response/BasicScopeResponse.java
@@ -1,4 +1,35 @@
 package com.amos2020.javabackend.rest_service.response;
 
+import com.amos2020.javabackend.entity.Scope;
+import lombok.Getter;
+import lombok.Setter;
+
 public class BasicScopeResponse {
+
+    public BasicScopeResponse(Scope scope){
+        this.auditId = scope.getAuditId();
+        this.faccritId = scope.getFaccritId();
+        this.changeNote = scope.getChangeNote();
+        this.removed = scope.getRemoved();
+        this.note = scope.getNote();
+    }
+    @Getter
+    @Setter
+    private int auditId;
+
+    @Getter
+    @Setter
+    private int faccritId;
+
+    @Getter
+    @Setter
+    private String changeNote;
+
+    @Getter
+    @Setter
+    private Boolean removed;
+
+    @Getter
+    @Setter
+    private String note;
 }
