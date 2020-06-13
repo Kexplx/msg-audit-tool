0a1c2ddb900f10e1f22f257f7e4ea375cd5f2e87
Oscar Rosner <oscar.rosner@web.de>
2020-06-13 23:05:47 +0200

    feat: update faccrit-examples

--- ng-frontend/src/app/core/data/examples/fac-crits.ts
+++ ng-frontend/src/app/core/data/examples/fac-crits.ts
@@ -3,17 +3,16 @@ import { generate } from 'shortid';
 
 export const FACCRITS: FacCrit[] = [
   { id: '1', name: 'FACTOR 1', referenceId: null },
-  { id: generate(), name: 'CRITERIA 1', referenceId: '1' },
-  { id: generate(), name: 'CRITERIA 2', referenceId: '1' },
-  { id: generate(), name: 'CRITERIA 3', referenceId: '1' },
 
   { id: '2', name: 'FACTOR 2', referenceId: null },
-  { id: generate(), name: 'CRITERIA 1', referenceId: '2' },
-  { id: generate(), name: 'CRITERIA 2', referenceId: '2' },
-  { id: generate(), name: 'CRITERIA 3', referenceId: '2' },
 
   { id: '3', name: 'FACTOR 3', referenceId: null },
   { id: generate(), name: 'CRITERIA 1', referenceId: '3' },
   { id: generate(), name: 'CRITERIA 2', referenceId: '3' },
   { id: generate(), name: 'CRITERIA 3', referenceId: '3' },
+
+  { id: '4', name: 'FACTOR 4', referenceId: null },
+  { id: generate(), name: 'CRITERIA 1', referenceId: '4' },
+  { id: generate(), name: 'CRITERIA 2', referenceId: '4' },
+  { id: generate(), name: 'CRITERIA 4', referenceId: '4' },
 ];
