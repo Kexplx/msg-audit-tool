1987fd0f385a4c463a9b36155616a427efdcca21
Oscar Rosner <oscar.rosner@web.de>
2020-06-15 21:17:35 +0200

    [102] Add path to interviews/:id/:id

--- ng-frontend/src/app/features/audit-overview/audit-overview-routing.module.ts
+++ ng-frontend/src/app/features/audit-overview/audit-overview-routing.module.ts
@@ -5,6 +5,7 @@ import { NewInterviewDialogComponent } from '../../shared/components/dialogs/new
 import { InterviewListComponent } from './components/audit-overview/interview-list/interview-list.component';
 import { AuditInfoComponent } from './components/audit-overview/audit-info/audit-info.component';
 import { EditAuditDialogComponent } from 'src/app/shared/components/dialogs/edit-audit-dialog/edit-audit-dialog.component';
+import { InterviewComponent } from './components/interview/interview.component';
 
 const routes: Routes = [
   {
@@ -25,6 +26,11 @@ const routes: Routes = [
         path: 'infos',
         component: AuditInfoComponent,
         children: [{ path: 'edit', component: EditAuditDialogComponent }],
+      },
+
+      {
+        path: 'interviews/:interviewId/:facCritId',
+        component: InterviewComponent,
       },
     ],
   },
