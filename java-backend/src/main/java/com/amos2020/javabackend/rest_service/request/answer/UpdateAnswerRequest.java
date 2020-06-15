32804af58ed3ff8f14e6e507e9af54e508bf6f60
Dennis Possart <dennis.possart@gmail.com>
2020-06-15 19:25:24 +0200

    [78] implement ScopeController

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/answer/UpdateAnswerRequest.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/answer/UpdateAnswerRequest.java
@@ -1,4 +1,58 @@
 package com.amos2020.javabackend.rest_service.request.answer;
 
-public class UpdateAnswerRequest {
+import com.amos2020.javabackend.rest_service.request.BasicRequest;
+import lombok.Getter;
+import lombok.Setter;
+
+public class UpdateAnswerRequest extends BasicRequest {
+
+    @Getter
+    @Setter
+    private int questionId;
+
+    @Getter
+    @Setter
+    private int interviewId;
+
+    @Getter
+    @Setter
+    private int faccritId;
+
+    @Getter
+    @Setter
+    private Boolean result;
+
+    @Getter
+    @Setter
+    private Boolean responsible;
+
+    @Getter
+    @Setter
+    private Boolean documentation;
+
+    @Getter
+    @Setter
+    private Boolean procedure;
+
+    @Getter
+    @Setter
+    private String reason;
+
+    @Getter
+    @Setter
+    private String proof;
+
+    @Getter
+    @Setter
+    private String annotation;
+
+    @Override
+    public void isValid() {
+        assertIdIsValid(questionId);
+        assertIdIsValid(interviewId);
+        assertIdIsValid(faccritId);
+        assertNoteIsValid(reason);
+        assertNoteIsValid(proof);
+        assertNoteIsValid(annotation);
+    }
 }
