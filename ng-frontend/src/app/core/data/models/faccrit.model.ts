abe43b99286fd16c7dde6d1679466e9af7e3d196
Oscar Rosner <oscar.rosner@web.de>
2020-06-16 18:22:48 +0200

    [102] Add Question model and add property to faccrit

--- ng-frontend/src/app/core/data/models/faccrit.model.ts
+++ ng-frontend/src/app/core/data/models/faccrit.model.ts
@@ -1,5 +1,8 @@
+import { Question } from './question.model';
+
 export interface FacCrit {
   id: string;
   referenceId: string;
   name: string;
+  questions: Question[];
 }
---
+++ ng-frontend/src/app/core/data/models/question.model.ts
@@ -0,0 +1,5 @@
+export interface Question {
+  id: string;
+  facCritId: string;
+  textDe: string;
+}
