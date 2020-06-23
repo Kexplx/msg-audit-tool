55bc4652c408ceca023285ccdb235ec6cb1d1608
Oscar Rosner <oscar.rosner@web.de>
2020-06-23 17:29:36 +0200

    [51] Add SidebarInterviewListComponent

--- ng-frontend/src/app/shared/sidebar/sidebar-interview-list/sidebar-interview-list.component.ts
+++ ng-frontend/src/app/shared/sidebar/sidebar-interview-list/sidebar-interview-list.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Select, Store } from '@ngxs/store';
+import { Store } from '@ngxs/store';
 import { AuditState } from 'src/app/core/ngxs/audit.state';
 import { Observable } from 'rxjs';
 import { Audit } from 'src/app/core/data/models/audit.model';
-import { NbMenuItem } from '@nebular/theme';
+import { NbMenuItem, NbMenuService } from '@nebular/theme';
 import { FactorsPipe } from '../../pipes/factors.pipe';
 import { CriteriaByFactorPipe } from '../../pipes/facCritByFactor.pipe';
 
@@ -14,39 +14,43 @@ import { CriteriaByFactorPipe } from '../../pipes/facCritByFactor.pipe';
 })
 export class SidebarInterviewListComponent implements OnInit {
   @Input() url: string;
-
-  audit: Observable<Audit>;
+  audit$: Observable<Audit>;
 
   constructor(
     private store: Store,
     private factorsPipe: FactorsPipe,
     private criteriaPipe: CriteriaByFactorPipe,
+    private menuService: NbMenuService,
   ) {}
 
   items: NbMenuItem[];
 
   ngOnInit() {
+    this.menuService.onItemClick().subscribe(x => {
+      const el = document.getElementById(x.item.data);
+      el?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    });
+
     const idRegex = /\/audits\/([^\/]*)\/.*/gm;
     const id = idRegex.exec(this.url)[1];
 
-    this.audit = this.store.select(AuditState.audit(id));
+    this.audit$ = this.store.select(AuditState.audit(id));
 
     this.items = [];
-    this.audit.subscribe(audit => {
-      console.log('Hello');
+    this.audit$.subscribe(audit => {
       if (!audit) return;
-      console.log('World');
 
       for (const factor of this.factorsPipe.transform(audit.scope)) {
         const criterias = this.criteriaPipe.transform(audit.scope, factor.id);
 
         const menuItem: NbMenuItem = {
           title: factor.name,
+          data: factor.id,
         };
 
-        if (criterias) {
+        if (criterias.length > 0) {
           menuItem.children = criterias.map(x => {
-            return { title: x.name };
+            return { title: x.name, data: x.id };
           });
         }
 
