ce5f57931ff6766773d73165b83bdd0c5692bd0e
Markus Goller <markus.goller@fau.de>
2020-06-25 12:20:27 +0200

    [145] Add Annotations and remove unnecessary asserts

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/audit/UpdateAuditRequest.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/audit/UpdateAuditRequest.java
@@ -1,31 +1,31 @@
 package com.amos2020.javabackend.rest_service.request.audit;
 
+import com.amos2020.javabackend.rest_service.Constants;
 import com.amos2020.javabackend.rest_service.request.BasicRequest;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.sql.Date;
 
 public class UpdateAuditRequest extends BasicRequest {
-
-    // mandatory
     @Getter
     @Setter
+    @NotNull
+    @NotBlank
+    @Size(min = 1, max = Constants.NAME_LENGTH)
     private String auditName;
-
-    // mandatory
     @Getter
     @Setter
+    @NotNull
     private Date startDate;
-
-    // Not mandatory
     @Getter
     @Setter
     private Date endDate;
 
     public void isValid() throws IllegalArgumentException {
-        assertNameIsValid(auditName);
-        assertDateIsNotNull(startDate);
         assertDatesAreValid(startDate, endDate);
     }
 }
