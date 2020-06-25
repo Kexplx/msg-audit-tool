87c5594f25ef9095c18e34888d2ca68b66a30a12
Markus Goller <markus.goller@fau.de>
2020-06-25 12:18:27 +0200

    [145] Add Constants for String lengths (256,1024, 8096)

---
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/Constants.java
@@ -0,0 +1,7 @@
+package com.amos2020.javabackend.rest_service;
+
+public class Constants {
+    public static final int NAME_LENGTH = 256;
+    public static final int NOTE_LENGTH = 1024;
+    public static final int TEXT_LENGTH = 8096;
+}
