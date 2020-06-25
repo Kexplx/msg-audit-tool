7a655aa9df26f6fdd908e584fcc85b665a1f0f92
Markus Goller <markus.goller@fau.de>
2020-06-25 17:43:41 +0200

    [145] Add example values for UpdateAuditContactPeopleRequest

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/audit/UpdateAuditContactPeopleRequest.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/audit/UpdateAuditContactPeopleRequest.java
@@ -1,6 +1,7 @@
 package com.amos2020.javabackend.rest_service.request.audit;
 
 import com.amos2020.javabackend.rest_service.request.BasicRequest;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,10 +13,12 @@ public class UpdateAuditContactPeopleRequest extends BasicRequest {
     @Setter
     @NotNull
     @Min(1)
+    @Schema(type = "Integer", name = "auditId", example = "1", required = true)
     private int auditId;
     @Getter
     @Setter
     @NotNull
     @Min(1)
+    @Schema(type = "Integer", name = "contactPersonId", example = "1", required = true)
     private int contactPersonId;
 }
