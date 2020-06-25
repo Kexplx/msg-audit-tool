4998a6956235cac73b513ee7bcbf7162c7b07680
Cathy Hu <cathy.hu@fau.de>
2020-06-25 19:53:06 +0200

    [129] Add fixed goals form

--- ng-frontend/src/app/features/audit-overview/components/interview/interview.component.ts
+++ ng-frontend/src/app/features/audit-overview/components/interview/interview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Interview } from 'src/app/core/data/models/interview.model';
@@ -38,5 +38,20 @@ export class InterviewComponent implements OnInit {
       this.facCrit$.subscribe(facCrit => facCrit ?? this.router.navigate(['/audits']));
       this.interview$.subscribe(interview => interview ?? this.router.navigate(['/audits']));
     });
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollPosition =
+      window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    const guardY = document.querySelector('#guard').getBoundingClientRect().y;
+    if (scrollPosition >= guardY) {
+      const goalHeight = document.querySelector('#goals-item').clientHeight + 30;
+      document.querySelector('#goals').classList.add('goals-not-at-top');
+      document.getElementById('pad').style.paddingTop = goalHeight.toString() + 'px';
+    } else {
+      document.querySelector('#goals').classList.remove('goals-not-at-top');
+      document.getElementById('pad').style.paddingTop = '0px';
+    }
   }
 }
