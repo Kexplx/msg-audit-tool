6b3d779ea7e4d404723276f01eed7adb108113fe
Markus Goller <markus.goller@fau.de>
2020-06-25 12:20:05 +0200

    [145] Add Annotations and remove unnecessary valid method

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/audit/UpdateAuditContactPeopleRequest.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/audit/UpdateAuditContactPeopleRequest.java
@@ -4,17 +4,18 @@ import com.amos2020.javabackend.rest_service.request.BasicRequest;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 public class UpdateAuditContactPeopleRequest extends BasicRequest {
     @Getter
     @Setter
+    @NotNull
+    @Min(1)
     private int auditId;
     @Getter
     @Setter
+    @NotNull
+    @Min(1)
     private int contactPersonId;
-
-    @Override
-    public void isValid() {
-        assertIdIsValid(auditId);
-        assertIdIsValid(contactPersonId);
-    }
 }
