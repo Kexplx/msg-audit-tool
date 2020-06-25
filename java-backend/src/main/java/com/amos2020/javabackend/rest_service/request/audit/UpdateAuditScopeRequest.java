8fa10f6089bef3e8eea1bea482a48ed3ef42a3ac
Markus Goller <markus.goller@fau.de>
2020-06-25 12:20:45 +0200

    [145] Add Annotations and remove unnecessary valid method

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/audit/UpdateAuditScopeRequest.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/audit/UpdateAuditScopeRequest.java
@@ -1,29 +1,34 @@
 package com.amos2020.javabackend.rest_service.request.audit;
 
+import com.amos2020.javabackend.rest_service.Constants;
 import com.amos2020.javabackend.rest_service.request.BasicRequest;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class UpdateAuditScopeRequest extends BasicRequest {
     @Getter
     @Setter
+    @NotNull
+    @Min(message = "facccrit ID", value = 1)
     private int facCritId;
 
     @Getter
     @Setter
+    @Size(message = "change note", max = Constants.NOTE_LENGTH)
     private String changeNote;
 
     @Getter
     @Setter
+    @NotNull
     private boolean removed;
 
     @Getter
     @Setter
+    @Size(message = "normal note", max = Constants.TEXT_LENGTH)
     private String note;
-
-    public void isValid() throws IllegalArgumentException {
-        assertNoteIsValid(changeNote);
-        assertIdIsValid(facCritId);
-        assertTextIsValid(note);
-    }
 }
