44b9bcf39d84be43126b2c21c3c5f93dd12e0a44
Markus Goller <markus.goller@fau.de>
2020-06-25 12:22:48 +0200

    [145] Remove unused import

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/audit/UpdateAuditScopeRequest.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/audit/UpdateAuditScopeRequest.java
@@ -5,7 +5,6 @@ import com.amos2020.javabackend.rest_service.request.BasicRequest;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
