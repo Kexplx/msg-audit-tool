3e8292218d267fe5c04a6165d8ca81ff5d93f55f
Markus Goller <markus.goller@fau.de>
2020-06-25 12:19:55 +0200

    [145] Add Annotations and remove unnecessary valid method

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/audit/DeleteAuditRequest.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/audit/DeleteAuditRequest.java
@@ -1,31 +1,32 @@
 package com.amos2020.javabackend.rest_service.request.audit;
 
-import com.amos2020.javabackend.entity.AuditStatus;
+import com.amos2020.javabackend.rest_service.Constants;
 import com.amos2020.javabackend.rest_service.request.BasicRequest;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.sql.Date;
 
 public class DeleteAuditRequest extends BasicRequest {
     @Getter
     @Setter
+    @NotNull
+    @Size(min = 1, max = Constants.NAME_LENGTH)
+    @NotBlank
     private String reason;
 
     @Getter
     @Setter
+    @Min(1)
+    @NotNull
     private Integer contactPerson;
 
     @Getter
     @Setter
+    @NotNull
     private Date date;
-
-    @Getter
-    final AuditStatus status = AuditStatus.CANCELED;
-
-    public void isValid() throws IllegalArgumentException {
-        assertNameIsValid(reason);
-        assertIdIsValid(contactPerson);
-        assertDateIsNotNull(date);
-    }
 }
