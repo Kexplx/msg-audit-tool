c80c78068e184bef448368338d3e5f2a1946359c
Oscar Rosner <oscar.rosner@web.de>
2020-06-27 23:25:04 +0200

    [REFACTOR] Reduce Redundance in action listener

--- ng-frontend/src/app/shared/directives/action-listener.directive.ts
+++ ng-frontend/src/app/shared/directives/action-listener.directive.ts
@@ -12,53 +12,79 @@ import {
   UpdateContactPerson,
   AddContactPerson,
 } from 'src/app/core/ngxs/actions/contact-person.action';
-
+import { UpdateAnswer, AddAnswer } from 'src/app/core/ngxs/actions/answer.actions';
+import { first, bufferTime, filter, map, timeout } from 'rxjs/operators';
+import { timer } from 'rxjs';
 @Directive({
   selector: '[appActionListener]',
 })
 export class ActionListenerDirective implements OnInit {
   constructor(private actions$: Actions, private toastrService: NbToastrService) {}
+  count = 0;
 
   /**
    * Listens to completed NGXS Actions and shows a Toast at the bottom right
    */
   ngOnInit() {
-    this.actions$.pipe(ofActionCompleted(AddAudit)).subscribe(x => {
-      this.showToast(`Neuen Audit: ${x.action.audit.name} erstellt`, 'checkmark-circle-2-outline');
-    });
-
-    this.actions$.pipe(ofActionCompleted(DeleteAudit)).subscribe(x => {
-      this.showToast(`Audit: ${x.action.audit.name} gelöscht`, 'trash-outline');
-    });
-
-    this.actions$.pipe(ofActionCompleted(UpdateAudit)).subscribe(x => {
-      this.showToast(`Audit: ${x.action.audit.name} bearbeitet`, 'edit-outline');
-    });
-
-    this.actions$.pipe(ofActionCompleted(AddInterview)).subscribe(x => {
-      this.showToast(`Neues Interview erstellt`, 'checkmark-circle-2-outline');
-    });
-
-    this.actions$.pipe(ofActionCompleted(DeleteContactPerson)).subscribe(x => {
-      this.showToast(
-        `Kontaktperson ${x.action.contactPerson.firstName} ${x.action.contactPerson.lastName} gelöscht`,
-        'trash-outline',
-      );
-    });
-
-    this.actions$.pipe(ofActionCompleted(UpdateContactPerson)).subscribe(x => {
-      this.showToast(
-        `Kontaktperson ${x.action.contactPerson.firstName} ${x.action.contactPerson.lastName} bearbeitet`,
-        'edit-outline',
-      );
-    });
-
-    this.actions$.pipe(ofActionCompleted(AddContactPerson)).subscribe(x => {
-      this.showToast(
-        `Kontaktperson ${x.action.contactPerson.firstName} ${x.action.contactPerson.lastName} erstellt`,
-        'checkmark-circle-2-outline',
-      );
-    });
+    this.actions$
+      .pipe(
+        filter((x: { action: Object; status: string }) => x.status === 'SUCCESSFUL'),
+        map(x => x.action),
+      )
+      .subscribe((action: any) => {
+        switch (true) {
+          case action instanceof AddAudit:
+            this.showToast(
+              `Neuen Audit: ${action.audit.name} erstellt`,
+              'checkmark-circle-2-outline',
+            );
+            break;
+          case action instanceof DeleteAudit:
+            this.showToast(`Audit: ${action.audit.name} gelöscht`, 'trash-outline');
+            break;
+          case action instanceof UpdateAudit:
+            this.showToast(`Audit: ${action.audit.name} bearbeitet`, 'edit-outline');
+            break;
+          case action instanceof AddInterview:
+            this.showToast(`Neues Interview erstellt`, 'checkmark-circle-2-outline');
+            break;
+          case action instanceof AddContactPerson:
+            this.showToast(
+              `Kontaktperson ${action.contactPerson.firstName} ${action.contactPerson.lastName} erstellt`,
+              'checkmark-circle-2-outline',
+            );
+            break;
+          case action instanceof DeleteContactPerson:
+            this.showToast(
+              `Kontaktperson ${action.contactPerson.firstName} ${action.contactPerson.lastName} gelöscht`,
+              'trash-outline',
+            );
+            break;
+          case action instanceof UpdateContactPerson:
+            this.showToast(`Audit: ${action.audit.name} gelöscht`, 'trash-outline');
+            this.showToast(
+              `Kontaktperson ${action.contactPerson.firstName} ${action.contactPerson.lastName} bearbeitet`,
+              'edit-outline',
+            );
+            break;
+          case action instanceof AddAnswer:
+            if (this.count === 0) {
+              this.showToast(`Antworten aktualisiert`, 'checkmark-circle-2-outline');
+              this.count = 99;
+              timer(1000).subscribe(() => (this.count = 0));
+            }
+            break;
+          case action instanceof UpdateAnswer:
+            if (this.count === 0) {
+              this.showToast(`Antworten aktualisiert`, 'checkmark-circle-2-outline');
+              this.count = 99;
+              timer(1000).subscribe(() => (this.count = 0));
+            }
+            break;
+          default:
+            break;
+        }
+      });
   }
 
   showToast(text: string, icon: string) {
