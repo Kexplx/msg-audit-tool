26a87c3d134ac0f9da767af3d78aeabaafbc59c2
Cathy Hu <cathy.hu@fau.de>
2020-06-27 15:37:16 +0200

    [105] Remove js of goals accordeon, add sticky position css-only version

--- ng-frontend/src/app/features/audit-overview/components/interview/interview.component.ts
+++ ng-frontend/src/app/features/audit-overview/components/interview/interview.component.ts
@@ -39,32 +39,4 @@ export class InterviewComponent implements OnInit {
       this.interview$.subscribe(interview => interview ?? this.router.navigate(['/audits']));
     });
   }
-
-  /**
-   * Handles the transformation of the goals accordeon from standard layout to a fixed position
-   */
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    // determine current position on page
-    const scrollPosition =
-      window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    // determine guard div position on page
-    const guardY = document.querySelector('#guard').getBoundingClientRect().y;
-    if (scrollPosition >= guardY) {
-      // get height and width of "goals-item" accordeon item to set the width and the height after the "goals" accordeon gets taken out of the grid
-      const goalHeight = document.querySelector('#goals-item').clientHeight;
-      const goalWidth = document.querySelector('#goals-item').clientWidth;
-      // add padding to the "pad" div with goalHeight to stop the body from shrinking
-      document.getElementById('pad').style.paddingTop = goalHeight.toString() + 'px';
-      // fix the width of the "goals" accordeon
-      document.getElementById('goals').style.width = goalWidth.toString() + 'px';
-      // add css class to "goals" accordeon to set position:fixed
-      document.querySelector('#goals').classList.add('goals-not-at-top');
-    } else {
-      // remove padding
-      document.getElementById('pad').style.paddingTop = '0px';
-      // remove css class with position:fixed
-      document.querySelector('#goals').classList.remove('goals-not-at-top');
-    }
-  }
 }
