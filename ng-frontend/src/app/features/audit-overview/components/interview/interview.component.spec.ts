92b7b5e491f41c6b85e7fb34f61e0d78229cef6b
Oscar Rosner <oscar.rosner@web.de>
2020-06-28 05:38:15 +0200

    [137] Provide FormsModule to testbed

--- ng-frontend/src/app/features/audit-overview/components/interview/interview.component.spec.ts
+++ ng-frontend/src/app/features/audit-overview/components/interview/interview.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { InterviewComponent } from './interview.component';
 import { RouterModule } from '@angular/router';
 import { CoreModule } from 'src/app/core/core.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('InterviewComponent', () => {
   let component: InterviewComponent;
@@ -11,7 +12,7 @@ describe('InterviewComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [InterviewComponent],
-      imports: [RouterModule.forRoot([]), CoreModule],
+      imports: [RouterModule.forRoot([]), CoreModule, FormsModule, ReactiveFormsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(InterviewComponent);
