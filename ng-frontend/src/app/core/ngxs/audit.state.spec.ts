93bdb298bd012ceff8dc8ae0268e6214b9d86e07
Oscar Rosner <oscar.rosner@web.de>
2020-06-28 05:31:57 +0200

    [TEST] Add unit test for updating an interview

--- ng-frontend/src/app/core/ngxs/audit.state.spec.ts
+++ ng-frontend/src/app/core/ngxs/audit.state.spec.ts
@@ -3,7 +3,8 @@ import { NgxsModule, Store } from '@ngxs/store';
 import { Audit, AuditStatus } from '../data/models/audit.model';
 import { AuditState } from './audit.state';
 import * as jasmine from 'karma-jasmine';
-import { AddAudit, DeleteAudit, AddInterview } from './actions/audit.actions';
+import { AddAudit, DeleteAudit, AddInterview, UpdateInterview } from './actions/audit.actions';
+import { Interview, InterviewStatus } from '../data/models/interview.model';
 
 describe('AuditState', () => {
   let store: Store;
@@ -39,7 +40,7 @@ describe('AuditState', () => {
       store.dispatch(new AddAudit(audit));
     }
     const auditToDelete = audits[0];
-    store.dispatch(new DeleteAudit(auditToDelete));
+    store.dispatch(new DeleteAudit(auditToDelete.id));
     let updatedAudits = store.selectSnapshot(AuditState.audits);
     expect(updatedAudits[0]).not.toEqual(auditToDelete);
   });
@@ -51,10 +52,37 @@ describe('AuditState', () => {
     let a: Audit;
     audit$.subscribe(x => (a = x));
 
-    store.dispatch(new AddInterview(audit, { contactPeople: null, facCrits: null, status: null }));
+    store.dispatch(
+      new AddInterview(audit.id, { contactPeople: null, facCrits: null, status: null }),
+    );
     audit$.subscribe(x => {
       expect(x.interviews.length).toEqual(1);
     });
+  });
+
+  it('should update an interview after UpdateInterivew action was dispatched', () => {
+    const interview: Interview = {
+      contactPeople: null,
+      facCrits: null,
+      status: null,
+      id: 'test123',
+    };
+    let auditId: string;
+
+    audit.interviews = [interview];
+    store.dispatch(new AddAudit(audit)).subscribe((a: Audit) => (auditId = a.id));
+
+    store
+      .dispatch(
+        new UpdateInterview(auditId, {
+          ...interview,
+          status: InterviewStatus.Finished,
+        }),
+      )
+      .subscribe(() => {
+        const updatedInterview = store.selectSnapshot(AuditState.interview(interview.id));
+        expect(updatedInterview.status).toEqual(InterviewStatus.Finished);
+      });
   });
 
   it('returns a interview when searched by id', () => {
