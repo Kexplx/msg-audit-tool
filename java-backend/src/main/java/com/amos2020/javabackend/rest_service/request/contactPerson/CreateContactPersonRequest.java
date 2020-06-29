9eebb60a2f4024565c32e7e46eb7ad958e9ae53c
beze-FAU <benjamin.zenke@fau.de>
2020-06-29 21:10:14 +0200

    [147] Add Annotations in CreateContactPersonRequest

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/contactPerson/CreateContactPersonRequest.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/contactPerson/CreateContactPersonRequest.java
@@ -1,9 +1,14 @@
 package com.amos2020.javabackend.rest_service.request.contactPerson;
 
 import com.amos2020.javabackend.entity.Salutation;
+import com.amos2020.javabackend.rest_service.Constants;
 import com.amos2020.javabackend.rest_service.request.BasicRequest;
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 public class CreateContactPersonRequest extends BasicRequest {
 
@@ -15,24 +20,39 @@ public class CreateContactPersonRequest extends BasicRequest {
     private String title;
     @Getter
     @Setter
+    @NotNull
+    @NotBlank
+    @Size(min = 1, max = Constants.NAME_LENGTH)
     private String forename;
     @Getter
     @Setter
+    @NotNull
+    @NotBlank
+    @Size(min = 1, max = Constants.NAME_LENGTH)
     private String surname;
     @Getter
     @Setter
+    @Size(max = Constants.NAME_LENGTH)
     private String contactInformation;
     @Getter
     @Setter
+    @NotNull
+    @NotBlank
+    @Size(min = 1, max = Constants.NAME_LENGTH)
     private String companyName;
     @Getter
     @Setter
+    @Size(max = Constants.NAME_LENGTH)
     private String department;
     @Getter
     @Setter
+    @Size(max = Constants.NAME_LENGTH)
     private String sector;
     @Getter
     @Setter
+    @NotNull
+    @NotBlank
+    @Size(min = 1, max = Constants.NAME_LENGTH)
     private String corporateDivision;
 
     public void isValid() {
