904ee801aef9a35b97b8e4c60dcc7a7608f5a970
Dennis Possart <dennis.possart@gmail.com>
2020-06-29 19:52:31 +0200

    [148] Add swagger api documentation tags in BasicScopeResponse

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/response/BasicScopeResponse.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/response/BasicScopeResponse.java
@@ -2,14 +2,16 @@ package com.amos2020.javabackend.rest_service.response;
 
 
 import com.amos2020.javabackend.entity.Scope;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 public class BasicScopeResponse {
 
     public BasicScopeResponse() {
-
+        //empty constructor
     }
+
     public BasicScopeResponse(int auditId, int facCritId, boolean removed, String change_note, String note) {
         this.auditId = auditId;
         this.facCritId = facCritId;
@@ -28,24 +30,26 @@ public class BasicScopeResponse {
 
     @Getter
     @Setter
+    @Schema(type = "Integer", name = "auditId", example = "123456")
     private int auditId;
+
     @Getter
     @Setter
+    @Schema(type = "Integer", name = "faccritId", example = "123456")
     private int facCritId;
 
     @Getter
     @Setter
+    @Schema(type = "boolean", name = "removed", example = "true")
     private boolean removed;
 
-
     @Getter
     @Setter
+    @Schema(type = "String", name = "change_note", example = "Some change note")
     private String change_note;
 
-
     @Getter
     @Setter
+    @Schema(type = "String", name = "note", example = "Some note")
     private String note;
-
-
 }
