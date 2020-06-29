4ee85019184dd6e465f3504c2a226c3235be1ce5
Markus Goller <markus.goller@fau.de>
2020-06-29 16:55:52 +0200

    [146] Add annotations instead of asserts and add examples in UpdateInterviewRequest

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/interview/UpdateInterviewRequest.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/interview/UpdateInterviewRequest.java
@@ -2,37 +2,41 @@ package com.amos2020.javabackend.rest_service.request.interview;
 
 import com.amos2020.javabackend.entity.InterviewStatus;
 import com.amos2020.javabackend.rest_service.request.BasicRequest;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.sql.Date;
 
 public class UpdateInterviewRequest extends BasicRequest {
 
     @Getter
     @Setter
+    @NotNull
+    @Schema(type = "string", name = "startDate", format = "date", required = true)
     private Date startDate;
     @Getter
     @Setter
+    @Schema(type = "string", name = "endDate", format = "date", required = true)
     private Date endDate;
     @Getter
     @Setter
+    @NotNull
+    @Schema(type = "String", name = "status", example = "ACTIVE", required = true)
     private InterviewStatus status;
     @Getter
     @Setter
+    @NotNull
+    @NotBlank
+    @Size(max = MAX_SMALL_TEXT_LENGTH)
+    @Schema(type = "String", name = "goal", example = "Das geänderte Ziel ist ...", required = true)
     private String goal;
 
 
     public void isValid() {
-        assertDateIsNotNull(startDate);
         assertDatesAreValid(startDate, endDate);
-        assertInterviewStatusIsNotNull(status);
-        assertNoteIsValid(goal);
-    }
-
-    private void assertInterviewStatusIsNotNull(InterviewStatus status) {
-        if (status == null) {
-            throw new IllegalArgumentException("Interview status can not be null!");
-        }
     }
 }
