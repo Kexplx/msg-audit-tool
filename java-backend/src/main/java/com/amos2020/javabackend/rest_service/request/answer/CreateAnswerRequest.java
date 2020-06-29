5a4a8bc78af96bac438b00a32f214f191186f0a7
Dennis Possart <dennis.possart@gmail.com>
2020-06-29 19:50:21 +0200

    [149] Add swagger api documentation and javax validation tags in CreateAnswerRequest

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/answer/CreateAnswerRequest.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/answer/CreateAnswerRequest.java
@@ -1,16 +1,28 @@
 package com.amos2020.javabackend.rest_service.request.answer;
 
 import com.amos2020.javabackend.rest_service.request.BasicRequest;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 public class CreateAnswerRequest extends BasicRequest {
 
     @Getter
     @Setter
+    @NotNull
+    @Min(message ="Question Id", value = 1)
+    @Schema(type = "Integer", name = "questionId", example = "1", required = true)
     private int questionId;
     @Getter
     @Setter
+    @NotNull
+    @Min(message ="Interview Id", value = 1)
+    @Schema(type = "Integer", name = "interviewId", example = "1", required = true)
     private int interviewId;
 
     public void isValid() {
