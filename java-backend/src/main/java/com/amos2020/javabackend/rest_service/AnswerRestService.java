b6a2be450ec5cd0e86292799fe890a352a67a09e
Dennis Possart <dennis.possart@gmail.com>
2020-06-29 19:48:44 +0200

    [149] Add swagger api documentation tags in AnswerRestService

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/AnswerRestService.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/AnswerRestService.java
@@ -4,6 +4,11 @@ import com.amos2020.javabackend.rest_service.controller.AnswerController;
 import com.amos2020.javabackend.rest_service.request.answer.CreateAnswerRequest;
 import com.amos2020.javabackend.rest_service.request.answer.UpdateAnswerRequest;
 import com.amos2020.javabackend.rest_service.response.BasicAnswerResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import javassist.NotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Tag(name = "Answer", description = "The endpoints for the Answer resource")
 public class AnswerRestService {
 
     private final AnswerController answerController;
@@ -25,6 +31,12 @@ public class AnswerRestService {
      * @param interviewId int
      * @return ResponseEntity containing a list with BasicAnswerResponses
      */
+    @Operation(summary = "Get all Answers by Interview id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Received Answers by Interview id"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content)
+    })
     @GetMapping("/answers/interview/{id}")
     public ResponseEntity<List<BasicAnswerResponse>> getAnswersByInterviewId(@PathVariable("id") int interviewId) {
         List<BasicAnswerResponse> response;
@@ -45,6 +57,12 @@ public class AnswerRestService {
      * @param questionId  int
      * @return ResponseEntity containing the Answer
      */
+    @Operation(summary = "Get Answer by Interview and Question id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Received answer"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content)
+    })
     @GetMapping("/answers/interview/{id1}/question/{id2}")
     public ResponseEntity<BasicAnswerResponse> getAnswerByIds(@PathVariable("id1") int interviewId,
                                                               @PathVariable("id2") int questionId) {
@@ -64,6 +82,12 @@ public class AnswerRestService {
      *
      * @return ResponseEntity with Answer
      */
+    @Operation(summary = "Create a new Answer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully created new Answer"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content)
+    })
     @PostMapping("/answers")
     public ResponseEntity<BasicAnswerResponse> createAnswer(@RequestBody CreateAnswerRequest request) {
         BasicAnswerResponse response;
@@ -86,22 +110,27 @@ public class AnswerRestService {
      * @param request
      * @return
      */
+    @Operation(summary = "Update an existing Answer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully updated Answer"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content)
+    })
     @PutMapping("/answers/interview/{id1}/question/{id2}")
     public ResponseEntity<BasicAnswerResponse> updateAnswer(@PathVariable("id1") int interviewId,
                                                             @PathVariable("id2") int questionId,
                                                             @RequestBody UpdateAnswerRequest request) {
         BasicAnswerResponse response;
-
         try {
             // Validate parameters for updating answer
             request.isValid();
             response = answerController.updateAnswer(interviewId, questionId, request.getResult(),
                     request.getResponsible(), request.getDocumentation(), request.getProcedure(),
                     request.getReason(), request.getProof(), request.getAnnotation());
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().build();
         } catch (NotFoundException e) {
             return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(response);
     }
