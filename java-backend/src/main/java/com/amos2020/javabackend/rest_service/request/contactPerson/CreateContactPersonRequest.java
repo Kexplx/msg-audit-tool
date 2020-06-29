d9ead2acb9685ae92a45f419ef7cf601ac65b95b
beze-FAU <benjamin.zenke@fau.de>
2020-06-29 21:22:18 +0200

    [147] Add examples in CreateContactPersonRequest

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/contactPerson/CreateContactPersonRequest.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/contactPerson/CreateContactPersonRequest.java
@@ -3,6 +3,7 @@ package com.amos2020.javabackend.rest_service.request.contactPerson;
 import com.amos2020.javabackend.entity.Salutation;
 import com.amos2020.javabackend.rest_service.Constants;
 import com.amos2020.javabackend.rest_service.request.BasicRequest;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,45 +15,54 @@ public class CreateContactPersonRequest extends BasicRequest {
 
     @Getter
     @Setter
+    @Schema(type = "String", name = "salutation", example = "MANN")
     private Salutation salutation;
     @Getter
     @Setter
+    @Schema(type = "String", name = "title", example = "Dr.")
     private String title;
     @Getter
     @Setter
     @NotNull
     @NotBlank
     @Size(min = 1, max = Constants.NAME_LENGTH)
+    @Schema(type = "String", name = "forename", example = "John", required = true)
     private String forename;
     @Getter
     @Setter
     @NotNull
     @NotBlank
     @Size(min = 1, max = Constants.NAME_LENGTH)
+    @Schema(type = "String", name = "surname", example = "Doe", required = true)
     private String surname;
     @Getter
     @Setter
     @Size(max = Constants.NAME_LENGTH)
+    @Schema(type = "String", name = "contactInformation", example = "john.doe@msg.de")
     private String contactInformation;
     @Getter
     @Setter
     @NotNull
     @NotBlank
     @Size(min = 1, max = Constants.NAME_LENGTH)
+    @Schema(type = "String", name = "companyName", example = "msg systems AG", required = true)
     private String companyName;
     @Getter
     @Setter
     @Size(max = Constants.NAME_LENGTH)
+    @Schema(type = "String", name = "department", example = "Softwareentwicklung")
     private String department;
     @Getter
     @Setter
     @Size(max = Constants.NAME_LENGTH)
+    @Schema(type = "String", name = "department", example = "Software")
     private String sector;
     @Getter
     @Setter
     @NotNull
     @NotBlank
     @Size(min = 1, max = Constants.NAME_LENGTH)
+    @Schema(type = "String", name = "corporateDivision", example = "msg Public Sector", required = true)
     private String corporateDivision;
 
     public void isValid() {
