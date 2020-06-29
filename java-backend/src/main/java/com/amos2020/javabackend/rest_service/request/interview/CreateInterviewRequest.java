9cbc05428e2200bbf3bcbc52c790ae2d05c969d5
Markus Goller <markus.goller@fau.de>
2020-06-29 16:55:26 +0200

    [146] Add annotations instead of asserts and add examples in CreateInterviewRequest

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/interview/CreateInterviewRequest.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/interview/CreateInterviewRequest.java
@@ -1,11 +1,15 @@
 package com.amos2020.javabackend.rest_service.request.interview;
 
 import com.amos2020.javabackend.rest_service.request.BasicRequest;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.sql.Date;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -13,29 +17,37 @@ public class CreateInterviewRequest extends BasicRequest {
 
     @Getter
     @Setter
+    @NotNull
+    @Min(1)
+    @Schema(type = "Integer", name = "auditId", example = "1", required = true)
     private int auditId;
     @Getter
     @Setter
+    @NotNull
+    @Schema(type = "string", name = "startDate", format = "date", required = true)
     private Date startDate;
+
     @Getter
     @Setter
+    @Schema(type = "string", name = "endDate", format = "date", required = true)
     private Date endDate;
     @Getter
     @Setter
+    @NotNull
+    @NotBlank
+    @Size(max = MAX_SMALL_TEXT_LENGTH)
+    @Schema(type = "String", name = "goal", example = "Das Ziel ist ...", required = true)
     private String goal;
     @Getter
     @Setter
-    private HashMap<Integer, String> interviewedPeople;
+    @Schema(type = "Array", name = "interviewedPeople", example = "{\"1\":" + "\"Software architect\"}", required = true)
+    private HashMap<@Min(1) Integer, @NotNull @NotBlank @Size(min = 1, max = 256) String> interviewedPeople;
     @Getter
     @Setter
-    private List<Integer> interviewScope;
+    @Schema(type = "Array", name = "interviewScope", example = "[1]", required = true)
+    private List<@Min(1) Integer> interviewScope;
 
     public void isValid() {
-        assertIdIsValid(auditId);
-        assertDateIsNotNull(startDate);
         assertDatesAreValid(startDate, endDate);
-        assertNoteIsValid(goal);
-        assertIdsAreValid(new ArrayList<>(interviewedPeople.keySet()));
-        assertIdsAreValid(interviewScope);
     }
 }
