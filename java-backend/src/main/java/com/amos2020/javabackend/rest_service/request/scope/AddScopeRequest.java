8d06c7968993b0591810fb9ecd333ede2752ffc0
Dennis Possart <dennis.possart@gmail.com>
2020-06-29 19:51:35 +0200

    [148] Add swagger api documentation and javax validation tags in AddScopeRequest

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/scope/AddScopeRequest.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/scope/AddScopeRequest.java
@@ -1,17 +1,19 @@
 package com.amos2020.javabackend.rest_service.request.scope;
 
 import com.amos2020.javabackend.rest_service.request.BasicRequest;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
 import java.util.List;
 
 public class AddScopeRequest extends BasicRequest {
 
     @Getter
     @Setter
-    private List<Integer> scope;
-
+    @Schema(type = "Array", name = "scope", example = "[1]", required = true)
+    private List<@Min(1)Integer> scope;
 
     public void isValid() {
         for (Integer i : scope) {
