c0588b757b148e822ba56c95fcac300421f4de52
Markus Goller <markus.goller@fau.de>
2020-06-29 16:55:41 +0200

    [146] Add annotations instead of asserts and add examples in InterviewAddContactPersonRequest

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/interview/InterviewAddContactPersonRequest.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/request/interview/InterviewAddContactPersonRequest.java
@@ -1,22 +1,30 @@
 package com.amos2020.javabackend.rest_service.request.interview;
 
 import com.amos2020.javabackend.rest_service.request.BasicRequest;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 
 public class InterviewAddContactPersonRequest extends BasicRequest {
 
     @Getter
     @Setter
+    @NotNull
+    @Min(1)
+    @Schema(type = "Integer", name = "contactPersonId", example = "1", required = true)
     private int contactPersonId;
 
     @Getter
     @Setter
+    @NotNull
+    @NotBlank
+    @Size(max = MAX_NAME_LENGTH)
+    @Schema(type = "String", name = "role", example = "Software architect", required = true)
     private String role;
-
-    public void isValid() {
-        assertIdIsValid(contactPersonId);
-        assertNameIsValid(role);
-    }
 }
