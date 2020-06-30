fed6bceb14aa873bae4885a847e7c4cd4aaed713
Markus Goller <markus.goller@fau.de>
2020-06-30 11:37:16 +0200

    [REFACTOR] Add java doc for AuditContactPersonService

--- java-backend/src/main/java/com/amos2020/javabackend/service/AuditContactPersonService.java
+++ java-backend/src/main/java/com/amos2020/javabackend/service/AuditContactPersonService.java
@@ -16,12 +16,24 @@ public class AuditContactPersonService {
         this.repository = repository;
     }
 
+    /**
+     * Create multiple AuditContactPerson object by auditId and a list of contactPerson ids
+     *
+     * @param auditId        int
+     * @param contactPersons List<Integer>
+     */
     public void createAuditContactPersons(int auditId, List<Integer> contactPersons) {
         for (int contactPersonId : contactPersons) {
             createAuditContactPerson(auditId, contactPersonId);
         }
     }
 
+    /**
+     * Create a AuditContactPerson by audit id and contactPerson id
+     *
+     * @param auditId         int
+     * @param contactPersonId int
+     */
     public void createAuditContactPerson(int auditId, int contactPersonId) {
         AuditContactPerson auditContactPerson = new AuditContactPerson();
         auditContactPerson.setAuditId(auditId);
@@ -29,11 +41,24 @@ public class AuditContactPersonService {
         repository.save(auditContactPerson);
     }
 
+    /**
+     * Delete a specific AuditContactPerson by a given audit id and a given contactPerson id
+     *
+     * @param auditId         int
+     * @param contactPersonId int
+     */
     @Transactional
     public void deleteByAuditIdAndContactPersonId(int auditId, int contactPersonId) {
         repository.deleteByAuditIdAndContactPersonId(auditId, contactPersonId);
     }
 
+    /**
+     * Check if a specific AuditContactPerson exists
+     *
+     * @param auditId         int
+     * @param contactPersonId int
+     * @return AuditContactPerson or null (if not found)
+     */
     @Transactional
     public AuditContactPerson exists(int auditId, int contactPersonId) {
         return repository.findFirstByAuditIdAndContactPersonId(auditId, contactPersonId);
