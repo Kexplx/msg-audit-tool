17a2d2d5fb9c690032470c031b729085dcae2d9a
Markus Goller <markus.goller@fau.de>
2020-06-30 11:34:13 +0200

    [Fix] Add BasicFacCritResponse for FacCrit Endpoint

---
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/response/BasicFacCritResponse.java
@@ -0,0 +1,27 @@
+package com.amos2020.javabackend.rest_service.response;
+
+import com.amos2020.javabackend.entity.FacCrit;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+public class BasicFacCritResponse {
+    @Getter
+    @Setter
+    @Schema(type = "Integer", name = "id", example = "15")
+    private int id;
+    @Getter
+    @Setter
+    @Schema(type = "Integer", name = "referenceId", example = "3")
+    private Integer referenceId;
+    @Getter
+    @Setter
+    @Schema(type = "String", name = "name", example = "Nützlichkeit")
+    private String name;
+
+    public BasicFacCritResponse(FacCrit facCrit) {
+        this.id = facCrit.getId();
+        this.referenceId = facCrit.getReferenceId();
+        this.name = facCrit.getName();
+    }
+}
