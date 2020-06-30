517bd44beffeb8144cb16c33877ddc8f729a141f
Markus Goller <markus.goller@fau.de>
2020-06-30 11:36:21 +0200

    [REFACTOR] Add java doc for QuestionService

--- java-backend/src/main/java/com/amos2020/javabackend/service/QuestionService.java
+++ java-backend/src/main/java/com/amos2020/javabackend/service/QuestionService.java
@@ -3,7 +3,6 @@ package com.amos2020.javabackend.service;
 import com.amos2020.javabackend.entity.Question;
 import com.amos2020.javabackend.repository.QuestionRepository;
 import javassist.NotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,14 +11,30 @@ import java.util.Optional;
 
 @Service
 public class QuestionService {
-    @Autowired
-    QuestionRepository repository;
+    final QuestionRepository repository;
 
+    public QuestionService(QuestionRepository repository) {
+        this.repository = repository;
+    }
+
+    /**
+     * Get a all Questions for a facCrit by the facCrit id
+     *
+     * @param faccritId int
+     * @return List<Question>
+     */
     @Transactional
     public List<Question> getQuestionsByFacCritId(int faccritId) {
         return repository.getQuestionsByFaccritId(faccritId);
     }
 
+    /**
+     * Get a question by id
+     *
+     * @param questionId int
+     * @return Question
+     * @throws NotFoundException If the question id is invalid and can not be found
+     */
     public Question getQuestionById(int questionId) throws NotFoundException {
         Optional<Question> question = repository.findById(questionId);
         if (!question.isPresent()) {
