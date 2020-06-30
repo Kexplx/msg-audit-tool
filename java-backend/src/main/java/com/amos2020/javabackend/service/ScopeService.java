259ca66a87105d017369b154fbb33aa61e2f8b6b
Markus Goller <markus.goller@fau.de>
2020-06-30 11:35:54 +0200

    [REFACTOR] Remove unused method and add java doc

--- java-backend/src/main/java/com/amos2020/javabackend/service/ScopeService.java
+++ java-backend/src/main/java/com/amos2020/javabackend/service/ScopeService.java
@@ -16,16 +16,38 @@ public class ScopeService {
         this.repository = repository;
     }
 
+    /**
+     * Create Scope objects by a given audit id and a given List of FacCrit ids
+     *
+     * @param auditId            int
+     * @param factorCriteriaList List<Integer>
+     */
     public void createScopeByFactorCriteriaList(int auditId, List<Integer> factorCriteriaList) {
         for (int factorCriteriaId : factorCriteriaList) {
             createScope(auditId, factorCriteriaId);
         }
     }
 
+    /**
+     * Create a single Scope obejct by audit id and facCrit id
+     *
+     * @param auditId          int
+     * @param factorCriteriaId int
+     */
     public void createScope(int auditId, int factorCriteriaId) {
         saveScope(auditId, factorCriteriaId, "", false, "");
     }
 
+    /**
+     * Method to save a new or existing Scope object
+     *
+     * @param auditId          int
+     * @param factorCriteriaId int
+     * @param changeNote       String
+     * @param removed          bool
+     * @param note             String
+     * @return Scope
+     */
     private Scope saveScope(int auditId, int factorCriteriaId, String changeNote, boolean removed, String note) {
         Scope scope = new Scope();
         scope.setAuditId(auditId);
@@ -36,20 +58,40 @@ public class ScopeService {
         return repository.save(scope);
     }
 
+    /**
+     * Find a Scope object by audit id and facCrit id
+     *
+     * @param auditId   int
+     * @param facCritId int
+     * @return Scope
+     */
     @Transactional
     public Scope findScopeItemByIds(int auditId, int facCritId) {
         return repository.findFirstByAuditIdAndFaccritId(auditId, facCritId);
     }
 
-    public List<Scope> getAll() {
-        return repository.findAll();
-    }
-
+    /**
+     * Find Scope objects by audit id
+     *
+     * @param auditId int
+     * @return List<Scope>
+     */
     @Transactional
     public List<Scope> findScopeItemsByAuditId(int auditId) {
-        return  repository.findAllByAuditId(auditId);
+        return repository.findAllByAuditId(auditId);
     }
 
+    /**
+     * Update an existing Scope object
+     *
+     * @param auditId    int
+     * @param facCritId  int
+     * @param changeNote String
+     * @param isRemoved  bool
+     * @param note       String
+     * @return Updated Scope object
+     * @throws IllegalAccessException If Scope object has already been removed
+     */
     public Scope updateScopeItem(int auditId, int facCritId, String changeNote, boolean isRemoved, String note) throws IllegalAccessException {
         Scope scopeItem = findScopeItemByIds(auditId, facCritId);
         if (scopeItem != null) {
