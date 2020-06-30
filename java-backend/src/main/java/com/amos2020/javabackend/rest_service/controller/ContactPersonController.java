9e7a0b154d6db73148f7af6e59b918937ca93f02
Markus Goller <markus.goller@fau.de>
2020-06-30 13:21:25 +0200

    [REFACTOR] Add java doc + Reformatted for ContactPersonController

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/controller/ContactPersonController.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/controller/ContactPersonController.java
@@ -19,16 +19,34 @@ public class ContactPersonController {
         this.contactPersonService = contactPersonService;
     }
 
+    /**
+     * Create a new contactPerson
+     *
+     * @param salutation         Salutation
+     * @param title              String
+     * @param forename           String
+     * @param surname            String
+     * @param companyName        String
+     * @param department         String
+     * @param sector             String
+     * @param corporate_division String
+     * @return Created ContactPerson
+     */
     public BasicContactPersonResponse createContactPerson(Salutation salutation, String title, String forename, String surname, String companyName, String department, String sector, String corporate_division) {
         // Create contact person and save in database
 
-        if(forename == null || surname == null || companyName == null || corporate_division == null )
-            throw  new IllegalArgumentException();
+        if (forename == null || surname == null || companyName == null || corporate_division == null)
+            throw new IllegalArgumentException();
 
         ContactPerson contactPerson = contactPersonService.createContactPerson(salutation, title, forename, surname, companyName, department, sector, corporate_division);
         return new BasicContactPersonResponse(contactPerson);
     }
 
+    /**
+     * Get all existing contactPersons
+     *
+     * @return List of all ContactPerson objects
+     */
     public List<BasicContactPersonResponse> getAllContactPersons() {
         List<BasicContactPersonResponse> response = new ArrayList<>();
         for (ContactPerson c : contactPersonService.getAll()) {
@@ -37,14 +55,37 @@ public class ContactPersonController {
         return response;
     }
 
+    /**
+     * Get a ContactPerson by an id
+     *
+     * @param contactPersonId int
+     * @return ContactPerson
+     * @throws NotFoundException If contactPerson id is invalid and can not be found
+     */
     public BasicContactPersonResponse getContactPersonById(int contactPersonId) throws NotFoundException {
         ContactPerson contactPerson = contactPersonService.getContactPersonById(contactPersonId);
         return new BasicContactPersonResponse(contactPerson);
     }
 
-    public BasicContactPersonResponse updateContactPerson(int contactPersonId, Salutation salutation, String title, String forename, String surname, String contactInformation, String companyName, String department, String sector, String corporate_division)  throws NotFoundException {
-        if(forename == null || surname == null || companyName == null || corporate_division == null )
-            throw  new IllegalArgumentException();
+    /**
+     * Update an existing contactPerson
+     *
+     * @param contactPersonId    int
+     * @param salutation         Salutation
+     * @param title              String
+     * @param forename           String
+     * @param surname            String
+     * @param contactInformation String
+     * @param companyName        String
+     * @param department         String
+     * @param sector             String
+     * @param corporate_division String
+     * @return Updated ContactPerson
+     * @throws NotFoundException If ContactPerson can not be found
+     */
+    public BasicContactPersonResponse updateContactPerson(int contactPersonId, Salutation salutation, String title, String forename, String surname, String contactInformation, String companyName, String department, String sector, String corporate_division) throws NotFoundException {
+        if (forename == null || surname == null || companyName == null || corporate_division == null)
+            throw new IllegalArgumentException();
 
         ContactPerson contactPerson = contactPersonService.getContactPersonById(contactPersonId);
         contactPerson.setSalutation(salutation);
@@ -56,6 +97,6 @@ public class ContactPersonController {
         contactPerson.setDepartment(department);
         contactPerson.setSector(sector);
         contactPerson.setCorporateDivision(corporate_division);
-        return  new BasicContactPersonResponse(contactPersonService.updateContactPerson(contactPerson));
+        return new BasicContactPersonResponse(contactPersonService.updateContactPerson(contactPerson));
     }
 }
