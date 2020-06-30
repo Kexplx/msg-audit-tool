82cbadddd35bd73e0c5d62762b65c061069e7748
Markus Goller <markus.goller@fau.de>
2020-06-30 13:21:06 +0200

    [REFACTOR] Add java doc + Reformatted for AuditController

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/controller/AuditController.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/controller/AuditController.java
@@ -31,6 +31,17 @@ public class AuditController {
         this.contactPersonService = contactPersonService;
     }
 
+    /**
+     * Create a new Audit
+     *
+     * @param auditName     String
+     * @param startDate     Date
+     * @param endDate       Date
+     * @param scope         List<Integer>
+     * @param contactPeople List<Integer>
+     * @return Created Audit
+     * @throws NotFoundException If a facCrit id or a contactPerson id is invalid and can not be found
+     */
     public BasicAuditResponse createAudit(String auditName, Date startDate, Date endDate, List<Integer> scope, List<Integer> contactPeople) throws NotFoundException {
         // Create audit and save in database
         Audit audit = auditService.createAudit(auditName, startDate, endDate);
@@ -42,6 +53,16 @@ public class AuditController {
         return new BasicAuditResponse(audit, facCritService.getAllById(scope), contactPersonService.getAllByIds(contactPeople));
     }
 
+    /**
+     * Update an existing audit
+     *
+     * @param auditId   int
+     * @param auditName String
+     * @param startDate Date
+     * @param endDate   Date
+     * @return Updated Audit
+     * @throws NotFoundException If the audit id is invalid and can not be found
+     */
     public BasicAuditResponse updateAudit(int auditId, String auditName, Date startDate, Date endDate) throws NotFoundException {
         // check if audit exists
         Audit audit = auditService.getAuditById(auditId);
@@ -53,6 +74,14 @@ public class AuditController {
         return buildBasicResponse(audit);
     }
 
+    /**
+     * Add a contactPerson to an existing audit
+     *
+     * @param auditId         int
+     * @param contactPersonId int
+     * @return Updated audit
+     * @throws NotFoundException If the audit id or the contactPerson id are invalid and can not be found
+     */
     public BasicAuditResponse addContactPersonToAudit(int auditId, int contactPersonId) throws NotFoundException {
         // check if audit exists
         Audit audit = auditService.getAuditById(auditId);
@@ -67,6 +96,14 @@ public class AuditController {
         return buildBasicResponse(audit);
     }
 
+    /**
+     * Remove a contactPerson from an audit
+     *
+     * @param auditId         int
+     * @param contactPersonId int
+     * @return Updated Audit
+     * @throws NotFoundException If the audit id or the contactPerson id are invalid and can not be found
+     */
     public BasicAuditResponse removeContactPersonFromAudit(int auditId, int contactPersonId) throws NotFoundException {
         // check if audit exists
         Audit audit = auditService.getAuditById(auditId);
@@ -81,6 +118,18 @@ public class AuditController {
         return buildBasicResponse(audit);
     }
 
+    /**
+     * Update the scope of an existing audit
+     *
+     * @param auditId    int
+     * @param facCritId  int
+     * @param changeNote String
+     * @param removed    bool
+     * @param note       String
+     * @return Updated Audit
+     * @throws NotFoundException      If audit id or faccrit id are invalid and can not be found
+     * @throws IllegalAccessException If the Audit has already been soft deleted
+     */
     public BasicAuditResponse updateAuditScope(int auditId, int facCritId, String changeNote, boolean removed, String note) throws NotFoundException, IllegalAccessException {
         Audit audit = auditService.getAuditById(auditId);
         facCritService.exists(facCritId);
@@ -94,6 +143,16 @@ public class AuditController {
         return buildBasicResponse(audit);
     }
 
+    /**
+     * Soft deleting an existing audit
+     *
+     * @param auditId          int
+     * @param cancellationDate Date
+     * @param reason           String
+     * @param contactPersonId  int
+     * @return Deleted Audit
+     * @throws NotFoundException If the audit id is invalid and can not be found
+     */
     public BasicAuditResponse softDeleteAudit(int auditId, Date cancellationDate, String reason, int contactPersonId) throws NotFoundException {
         Audit audit = auditService.getAuditById(auditId);
         if (audit.getStatus().equals(AuditStatus.CANCELED))
@@ -112,11 +171,23 @@ public class AuditController {
         return buildBasicResponse(audit);
     }
 
+    /**
+     * Get an audit by audit id
+     *
+     * @param auditId int
+     * @return Audit
+     * @throws NotFoundException If the audit id is invalid and can not be found
+     */
     public BasicAuditResponse getAuditById(int auditId) throws NotFoundException {
         Audit audit = auditService.getAuditById(auditId);
         return buildBasicResponse(audit);
     }
 
+    /**
+     * Get all exiting auditd
+     *
+     * @return List of all audits
+     */
     public List<BasicAuditResponse> getAllAudits() throws NotFoundException {
         List<BasicAuditResponse> response = new ArrayList<>();
         for (Audit a : auditService.getAll()) {
@@ -125,6 +196,13 @@ public class AuditController {
         return response;
     }
 
+    /**
+     * Helper method for creating a BasicAuditResponse by a given audit
+     *
+     * @param audit Audit
+     * @return BasicAuditResponse
+     * @throws NotFoundException If a id is invalid and can not be found
+     */
     private BasicAuditResponse buildBasicResponse(Audit audit) throws NotFoundException {
         List<Integer> facCritIds = new ArrayList<>();
         audit.getScopesById().stream().filter(item -> !item.getRemoved()).forEach(item -> facCritIds.add(item.getFaccritId()));
