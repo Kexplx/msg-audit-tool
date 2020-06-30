7c7186e88661275455e0f1210a24ff51b324f40b
Markus Goller <markus.goller@fau.de>
2020-06-30 11:33:41 +0200

    [Fix] Add FacCritController for processing the getAllFacCrits request

---
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/controller/FacCritController.java
@@ -0,0 +1,32 @@
+package com.amos2020.javabackend.rest_service.controller;
+
+import com.amos2020.javabackend.entity.FacCrit;
+import com.amos2020.javabackend.rest_service.response.BasicFacCritResponse;
+import com.amos2020.javabackend.service.FacCritService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class FacCritController {
+
+    final FacCritService facCritService;
+
+    public FacCritController(FacCritService facCritService) {
+        this.facCritService = facCritService;
+    }
+
+    /**
+     * Get all existing facCrits
+     *
+     * @return all facCrits
+     */
+    public List<BasicFacCritResponse> getAllFacCrits() {
+        List<BasicFacCritResponse> responses = new ArrayList<>();
+        for (FacCrit facCrit : facCritService.getAllFacCrits()) {
+            responses.add(new BasicFacCritResponse(facCrit));
+        }
+        return responses;
+    }
+}
