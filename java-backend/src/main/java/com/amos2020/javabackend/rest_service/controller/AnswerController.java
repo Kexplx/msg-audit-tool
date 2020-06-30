ca9b0caac8970e156dc39ca917e21ef994aff3fd
Markus Goller <markus.goller@fau.de>
2020-06-30 13:20:34 +0200

    [REFACTOR] Add java doc + Reformatted for AnswerController

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/controller/AnswerController.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/controller/AnswerController.java
@@ -25,18 +25,45 @@ public class AnswerController {
         this.questionService = questionService;
     }
 
+    /**
+     * Get all answers for an interview
+     *
+     * @param interviewId int
+     * @return List of Answers
+     * @throws NotFoundException If interview can not be found
+     */
     public List<BasicAnswerResponse> getAllAnswersByInterviewId(int interviewId) throws NotFoundException {
         List<BasicAnswerResponse> response = new ArrayList<>();
+        interviewService.getInterviewById(interviewId);
         List<Answer> answers = answerService.getAnswersByInterviewId(interviewId);
         answers.forEach(a -> response.add(new BasicAnswerResponse(a)));
         return response;
     }
 
+    /**
+     * Get an answer by interview id and question id
+     *
+     * @param interviewId int
+     * @param questionId  int
+     * @return Answer
+     * @throws NotFoundException If interview id or question id is invalid and can not be found
+     */
     public BasicAnswerResponse getAnswerByIds(int interviewId, int questionId) throws NotFoundException {
         Answer answer = answerService.getAnswerByIds(questionId, interviewId);
+        if (answer == null) {
+            throw new NotFoundException("Answer can not be found");
+        }
         return new BasicAnswerResponse(answer);
     }
 
+    /**
+     * Create a new answer
+     *
+     * @param interviewId int
+     * @param questionId  int
+     * @return Created Answer
+     * @throws NotFoundException If interview or question can not be found
+     */
     public BasicAnswerResponse createAnswer(int interviewId, int questionId) throws NotFoundException {
         interviewService.getInterviewById(interviewId);
         Question question = questionService.getQuestionById(questionId);
@@ -45,6 +72,21 @@ public class AnswerController {
         return new BasicAnswerResponse(answer);
     }
 
+    /**
+     * Update an existing answer
+     *
+     * @param interviewId   int
+     * @param questionId    int
+     * @param result        bool
+     * @param responsible   bool
+     * @param documentation bool
+     * @param procedure     bool
+     * @param reason        String
+     * @param proof         String
+     * @param annotation    String
+     * @return Updated Answer
+     * @throws NotFoundException If an id is invalid and can not be found
+     */
     public BasicAnswerResponse updateAnswer(int interviewId,
                                             int questionId,
                                             Boolean result,
@@ -53,8 +95,11 @@ public class AnswerController {
                                             Boolean procedure,
                                             String reason,
                                             String proof,
-                                            String annotation) throws NotFoundException{
+                                            String annotation) throws NotFoundException {
         Answer answer = answerService.getAnswerByIds(questionId, interviewId);
+        if (answer == null) {
+            throw new NotFoundException("Answer can not be found");
+        }
         answer.setResult(result);
         answer.setResponsible(responsible);
         answer.setDocumentation(documentation);
