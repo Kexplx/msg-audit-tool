6af4b7031b66a051489427f29f7395352bb45b11
Oscar Rosner <oscar.rosner@web.de>
2020-07-02 00:18:15 +0200

    [114] Add InterviewDto

---
+++ ng-frontend/src/app/core/http/dtos/interview.dto.ts
@@ -0,0 +1,14 @@
+import { InterviewStatus } from '../../data/models/interview.model';
+import { Answer } from '../../data/models/answer.model';
+import { ContactPerson } from '../../data/models/contact-person.model';
+
+export interface InterviewDto {
+  interviewId?: number;
+  auditId: number;
+  startDate: string;
+  endDate: string;
+  goal: string;
+  status: InterviewStatus;
+  answers: Answer[];
+  interviewedPeople: ContactPerson[];
+}
