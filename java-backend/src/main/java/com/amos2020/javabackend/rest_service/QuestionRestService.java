4ec5883b689891200252ada460a1e047dac3f5d7
Dennis Possart <dennis.possart@gmail.com>
2020-07-02 08:40:37 +0200

    [FIX] Fix missing import for @CrossOrigin annotation

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/QuestionRestService.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/QuestionRestService.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import javassist.NotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
