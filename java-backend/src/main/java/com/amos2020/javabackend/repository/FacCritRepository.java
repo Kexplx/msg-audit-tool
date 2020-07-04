3194fbf676e8891519337d3c4a1eabe54402dce5
Dennis Possart <dennis.possart@gmail.com>
2020-07-04 23:40:19 +0200

    [154] Implement getFacCritsByInterviewId in FacCritRepository

--- java-backend/src/main/java/com/amos2020/javabackend/repository/FacCritRepository.java
+++ java-backend/src/main/java/com/amos2020/javabackend/repository/FacCritRepository.java
@@ -2,7 +2,15 @@ package com.amos2020.javabackend.repository;
 
 import com.amos2020.javabackend.entity.FacCrit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface FacCritRepository extends JpaRepository<FacCrit, Integer> {
+
+    @Query("select f from FacCrit f join Answer a on f.id= a.faccritId where a.interviewId=:interviewId")
+    List<FacCrit> getFacCritsByInterviewId(@Param("interviewId") int interviewId);
+
 }
 
