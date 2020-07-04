e8c8077e3ba70653b0f2d97fd773052f33d7d4c7
Oscar Rosner <oscar.rosner@web.de>
2020-07-04 09:42:47 +0200

    [115] Add goal property

--- ng-frontend/src/app/core/http/dtos/post-interview.dto.ts
+++ ng-frontend/src/app/core/http/dtos/post-interview.dto.ts
@@ -1,6 +1,7 @@
 export interface PostInterviewDto {
   auditId: number;
   startDate: string;
+  goal: string;
   interviewedContactPersons: { id: number; role: string }[];
   interviewScope: number[];
 }
