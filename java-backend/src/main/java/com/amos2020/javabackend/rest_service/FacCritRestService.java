ea02a191d82e42ef5b8cf1c30034e6127a78c20e
Dennis Possart <dennis.possart@gmail.com>
2020-07-04 23:41:25 +0200

    [154] Implement GET endpoint getAllFacCritsByInterviewId in FacCritRestService

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/FacCritRestService.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/FacCritRestService.java
@@ -3,12 +3,14 @@ package com.amos2020.javabackend.rest_service;
 import com.amos2020.javabackend.rest_service.controller.FacCritController;
 import com.amos2020.javabackend.rest_service.response.BasicFacCritResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -36,5 +38,21 @@ public class FacCritRestService {
     @GetMapping("/facCrits")
     public ResponseEntity<List<BasicFacCritResponse>> getAllFacCrits() {
         return ResponseEntity.ok(facCritController.getAllFacCrits());
+    }
+
+    @Operation(summary = "Get all Faccrits logicly ordered that are used in a specific Interview")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Receive all existing facCrits"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+    })
+    @GetMapping("/facCrits/interview/{id}")
+    public ResponseEntity<List<BasicFacCritResponse>> getAllFacCritsByInterviewId(@PathVariable("id") int interviewId) {
+        List<BasicFacCritResponse> response;
+        //try{
+            response = facCritController.getAllFacCritsByInterviewId(interviewId);
+        //}catch(Exception e){
+        //    return ResponseEntity.badRequest().build();
+        //}
+        return ResponseEntity.ok(response);
     }
 }
