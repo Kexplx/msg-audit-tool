a379393bba733c4e0ca17ee19fc3a4b73d2f49cf
Oscar Rosner <oscar.rosner@web.de>
2020-07-05 18:10:00 +0200

    [115] Implement InterviewDirectorComponent

--- ng-frontend/src/app/features/audit-overview/components/interview/interview-director/interview-director.component.ts
+++ ng-frontend/src/app/features/audit-overview/components/interview/interview-director/interview-director.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject, Observable, combineLatest } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
-import { Store } from '@ngxs/store';
-import { UpdateInterview } from 'src/app/core/ngxs/actions/inteview.actions';
+import { Store, Select } from '@ngxs/store';
+import { AppRouterState } from 'src/app/core/ngxs/app-router.state';
+import { FacCrit } from 'src/app/core/data/models/faccrit.model';
+import { Interview } from 'src/app/core/data/models/interview.model';
+import { AuditState } from 'src/app/core/ngxs/audit.state';
+import { InterviewState } from 'src/app/core/ngxs/interview.state';
+import { CoreService } from 'src/app/core/http/core.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-interview-director',
@@ -10,22 +16,42 @@ import { UpdateInterview } from 'src/app/core/ngxs/actions/inteview.actions';
   styleUrls: ['./interview-director.component.scss'],
 })
 export class InterviewDirectorComponent implements OnInit {
-  constructor(private store: Store) {}
+  @Select(AppRouterState.interviewId) interviewId$: Observable<number>;
+  @Select(AppRouterState.facCritId) facCritId$: Observable<number>;
+
+  facCritIds: number[];
+  facCrit$: Observable<FacCrit>;
+  interview$: Observable<Interview>;
+
+  constructor(
+    private store: Store,
+    private activatedRoute: ActivatedRoute,
+    private coreService: CoreService,
+    private router: Router,
+  ) {}
 
   ngOnInit() {
-    this.goalDebounce$.pipe(debounceTime(1000)).subscribe(goal => {
-      // this.store.dispatch(new UpdateInterview())
+    combineLatest([this.interviewId$, this.facCritId$]).subscribe(ids => {
+      this.facCrit$ = this.store.select(AuditState.facCrit(ids[1]));
+      this.interview$ = this.store.select(InterviewState.interview(ids[0]));
 
-      console.log(goal);
+      this.coreService.getFacCritsByInterviewId(ids[0]).subscribe((facCrits: FacCrit[]) => {
+        const result = (this.facCritIds = facCrits.map(f => f.id));
+        this.facCritIds = this.getDistinctIds(result);
+      });
+    });
+
+    this.goalDebounce$.pipe(debounceTime(1000)).subscribe(() => {
+      // this.store.dispatch(new UpdateInterview())
     });
   }
 
-  goal = 'Hello World';
   goalDebounce$ = new Subject<string>();
 
   onGoalInput(value: string) {
     this.goalDebounce$.next(value);
   }
+
   onNavigateForward(facCritId: number) {
     const indexOfFacCrit = this.facCritIds.indexOf(facCritId);
 
@@ -46,4 +72,15 @@ export class InterviewDirectorComponent implements OnInit {
     }
   }
 
+  getDistinctIds(ids: number[]) {
+    const distinctIds: number[] = [];
+
+    for (const id of ids) {
+      if (!distinctIds.includes(id)) {
+        distinctIds.push(id);
+      }
+    }
+
+    return distinctIds;
+  }
 }
