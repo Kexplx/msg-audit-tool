32173bb9b6eff0963885030e4c2c8318d1f05c74
Cathy Hu <cathy.hu@fau.de>
2020-07-06 21:41:23 +0200

    [152] Add new tests for edit-audit-dialog

--- ng-frontend/cypress/integration/dialogs/edit-audit-dialog.spec.ts
+++ ng-frontend/cypress/integration/dialogs/edit-audit-dialog.spec.ts
@@ -1,13 +1,16 @@
-/**
- * Tests the dialog to edit an audit.
- *
- * Hint: Since the dialogs for editing and adding audits are the same, we don't test for the
- * form here explicitly. These explicit tests can be found in the add-audit-dialog-tests.
- * The form input is done the same way in both edit/add-testsgroups with
- * cy.addAudit() so it will implicitely guarantee the form is inputtable and working.
- */
 describe('EditAuditDialog', () => {
   const auditsUrl = Cypress.config().baseUrl + '/audits';
+  let audits;
+  let testAudit;
+
+  before(() => {
+    cy.fixture('backend-mock-data/audits.json').then(json => {
+      audits = json;
+    });
+    cy.fixture('user-input-data/example-audit').then(json => {
+      testAudit = json;
+    });
+  });
 
   beforeEach(() => {
     cy.injectBackendMocks();
@@ -22,14 +25,78 @@ describe('EditAuditDialog', () => {
   });
 
   context('When focussing on the existing form content it ...', () => {
-    it('shows the correct audit name', () => {});
-    it('shows the correct start and end date', () => {});
-    it('shows the correct contact person(s)', () => {});
-    it('shows the correct scope', () => {});
+    let audit;
+
+    beforeEach(() => {
+      audit = audits[0];
+      cy.visit(`${auditsUrl}/${audit.auditId}/edit`);
+    });
+
+    it('shows the correct audit name', () => {
+      cy.get('[data-cy=audit-name-input]').should('have.value', audit.auditName);
+    });
+
+    it('shows the correct start date', () => {
+      cy.get('[data-cy=audit-start-input]')
+        .invoke('val')
+        .then(val => {
+          assert.equal(
+            new Date(val).toLocaleDateString(),
+            new Date(audit.startDate).toLocaleDateString(),
+          );
+        });
+    });
+
+    it('shows the correct end date', () => {
+      cy.get('[data-cy=audit-end-input]')
+        .invoke('val')
+        .then(val => {
+          assert.equal(
+            new Date(val).toLocaleDateString(),
+            new Date(audit.endDate).toLocaleDateString(),
+          );
+        });
+    });
+
+    it('shows the correct contact person(s)', () => {
+      cy.get('[data-cy=audit-contacts]').click();
+      cy.get('[data-cy=audit-contact]').each((contact, i) => {
+        cy.wrap(contact).should('contain', audit.contactPersons[i].forename);
+        cy.wrap(contact).should('contain', audit.contactPersons[i].surname);
+      });
+      cy.get('[data-cy=audit-contacts]').click();
+    });
+
+    it('shows the correct scope', () => {
+      audit.scope.forEach(scope => {
+        cy.get('[data-cy=criteria-entry]')
+          .contains(scope.name)
+          .then(a => {
+            cy.wrap(a).get('.label > .custom-checkbox').should('have.class', 'checked');
+          });
+      });
+    });
   });
 
   context('When focussing on the network request it ...', () => {
-    it('builds a valid post request as form', () => {});
+    let audit;
+
+    beforeEach(() => {
+      audit = audits[0];
+      cy.visit(auditsUrl);
+      cy.visit(`${auditsUrl}/${audit.auditId}/edit`);
+    });
+
+    it('builds a valid post request as form', () => {
+      cy.inputAudit(testAudit);
+      cy.wait('@putAudits').then(xhr => {
+        assert.deepEqual(xhr.request.body, {
+          auditName: testAudit.name,
+          endDate: audit.endDate,
+          startDate: audit.startDate,
+        });
+      });
+    });
     it('shows error message when the network connection/requests failed', () => {});
   });
 });
