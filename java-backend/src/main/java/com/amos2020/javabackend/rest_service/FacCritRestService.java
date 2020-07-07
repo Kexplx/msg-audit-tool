ebaf9568b7de01e028f92cd1f530d80e888bde06
Dennis Possart <dennis.possart@gmail.com>
2020-07-07 15:14:47 +0200

    [156] Add validation and javadoc annotation in FacCritRestService

--- java-backend/src/main/java/com/amos2020/javabackend/rest_service/FacCritRestService.java
+++ java-backend/src/main/java/com/amos2020/javabackend/rest_service/FacCritRestService.java
@@ -3,19 +3,23 @@ package com.amos2020.javabackend.rest_service;
 import com.amos2020.javabackend.rest_service.controller.FacCritController;
 import com.amos2020.javabackend.rest_service.response.BasicFacCritResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.Min;
 import java.util.List;
 
 @RestController
+@Validated
 @Tag(name = "Factor / Criteria", description = "The endpoints for the facCrit resource")
 @CrossOrigin
 public class FacCritRestService {
@@ -40,19 +44,25 @@ public class FacCritRestService {
         return ResponseEntity.ok(facCritController.getAllFacCrits());
     }
 
-    @Operation(summary = "Get all Faccrits logicly ordered that are used in a specific Interview")
+    /**
+     * GET Endpoint for all distinct used Faccrits in a specific Interview
+     *
+     * @param interviewId
+     * @return ResponseEntity with a sorted List of Faccrits
+     */
+    @Operation(summary = "Get all distinct Faccrits that are used in a specific Interview")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Receive all existing facCrits"),
             @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
     })
     @GetMapping("/faccrits/interview/{id}")
-    public ResponseEntity<List<BasicFacCritResponse>> getAllFacCritsByInterviewId(@PathVariable("id") int interviewId) {
+    public ResponseEntity<List<BasicFacCritResponse>> getAllFacCritsByInterviewId(@PathVariable("id") @Parameter(name = "id", example = "1") @Min(1) int interviewId) {
         List<BasicFacCritResponse> response;
-        //try{
+        try{
             response = facCritController.getAllFacCritsByInterviewId(interviewId);
-        //}catch(Exception e){
-        //    return ResponseEntity.badRequest().build();
-        //}
+        }catch(Exception e){
+            return ResponseEntity.badRequest().build();
+        }
         return ResponseEntity.ok(response);
     }
 }
