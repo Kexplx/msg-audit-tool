42d52dc6e795700e54d2278d602ed98a7dbf8826
Dennis Possart <dennis.possart@gmail.com>
2020-07-12 20:45:53 +0200

    [FIX] Add missing ContactPersonRestServiceTest.class in RestServiceTests

--- java-backend/src/test/java/com/amos2020/javabackend/rest_service/RestServiceTests.java
+++ java-backend/src/test/java/com/amos2020/javabackend/rest_service/RestServiceTests.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({AnswerRestServiceTest.class, AuditRestServiceTest.class,
+@Suite.SuiteClasses({AnswerRestServiceTest.class, AuditRestServiceTest.class, ContactPersonRestServiceTest.class,
         InterviewRestServiceTest.class, ScopeRestServiceTest.class
 })
 public class RestServiceTests {
