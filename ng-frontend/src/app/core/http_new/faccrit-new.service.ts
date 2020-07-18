79a5a53f47ea77ce3c78eaa62266ca8d9290027e
Oscar Rosner <oscar.rosner@web.de>
2020-07-19 01:03:23 +0200

    [REFACTOR] Replace faccrit group method made by http call with local function

--- ng-frontend/src/app/core/http_new/faccrit-new.service.ts
+++ ng-frontend/src/app/core/http_new/faccrit-new.service.ts
@@ -22,7 +22,42 @@ export class FacCritNewService {
     });
   }
 
-  getFacCritsByInterviewId(id: number): Observable<FacCrit[]> {
-    return this.http.get<FacCrit[]>(environment.baseUrl + 'faccrits/interview/' + id);
+  groupedFacCrits(facCritIds: number[]) {
+    const facCrits: FacCrit[] = this._facCrits$.value.filter(fc => facCritIds.includes(fc.id));
+    const result: FacCrit[] = [];
+
+    for (const factor of facCrits.filter(fc => !fc.referenceId)) {
+      const criteriaOfFactor = facCrits
+        .filter(fc => fc.referenceId === factor.id)
+        .sort((a, b) => a.id - b.id);
+      result.push(factor, ...criteriaOfFactor);
+    }
+
+    for (const criteria of facCrits.filter(fc => fc.referenceId)) {
+      if (!result.find(fc => fc === criteria)) {
+        result.push(criteria);
+      }
+    }
+
+    result.sort((a, b) => {
+      if (a.referenceId && b.referenceId) return a.referenceId - b.referenceId;
+      if (!a.referenceId && b.referenceId) return a.id - b.referenceId;
+      if (a.referenceId && !b.referenceId) return a.referenceId - b.id;
+      if (!a.referenceId && !b.referenceId) return a.id - b.id;
+    });
+
+    return this.distinctFacCrits(result);
+  }
+
+  private distinctFacCrits(facCrits: FacCrit[]) {
+    const distinctFacCrits: FacCrit[] = [];
+
+    for (const facCrit of facCrits) {
+      if (!distinctFacCrits.find(fc => fc.id === facCrit.id)) {
+        distinctFacCrits.push(facCrit);
+      }
+    }
+
+    return distinctFacCrits;
   }
 }
