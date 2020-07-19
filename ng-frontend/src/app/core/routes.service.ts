be18cfd5a3a4a857b68647a31abb4460eb623a95
Oscar Rosner <oscar.rosner@web.de>
2020-07-20 00:28:33 +0200

    [REFACTOR] Add service for checking the current view

---
+++ ng-frontend/src/app/core/routes.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RoutesService {
+  inInterviewsList$ = new Subject<boolean>();
+  inInterview$ = new Subject<boolean>();
+
+  constructor(private router: Router) {
+    this.listenToNaviagtion();
+  }
+
+  private listenToNaviagtion(): void {
+    const inInterviewListRegex = /^\/audits\/[^\/]*\/interviews(\/new){0,1}$/;
+    const inInterviewRegex = /^\/audits\/[^\/]*\/interviews\/[^\/]*\/[^\/]*$/;
+
+    this.router.events
+      ?.pipe(filter(obj => obj instanceof NavigationEnd))
+      .subscribe(({ url }: NavigationEnd) => {
+        this.inInterviewsList$.next(inInterviewListRegex.test(url));
+        this.inInterview$.next(inInterviewRegex.test(url));
+      });
+  }
+}
