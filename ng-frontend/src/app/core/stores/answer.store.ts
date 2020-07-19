5d0c54943222808a68d4b4c6c62ff1a4a9eb1a96
Oscar Rosner <oscar.rosner@web.de>
2020-07-19 19:07:48 +0200

    [REFACTOR] Add loadAnswersByInterviewId

--- ng-frontend/src/app/core/stores/answer.store.ts
+++ ng-frontend/src/app/core/stores/answer.store.ts
@@ -1,29 +1,41 @@
 import { Injectable } from '@angular/core';
 import { Answer } from '../data/models/answer.model';
 import { AnswerService } from '../http/answer.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AnswerStore {
-  answers: Answer[] = [];
+  private readonly _answers$ = new BehaviorSubject<Answer[]>(null);
+
+  get answers$() {
+    return this._answers$.asObservable();
+  }
 
   constructor(private answerService: AnswerService) {}
 
-  setAnswers(answers: Answer[]) {
-    this.answers = answers;
+  loadAnswersByInterviewId(id: number) {
+    this.answerService
+      .getAnswersByInterviewId(id)
+      .subscribe(answers => this._answers$.next(answers));
   }
 
   updateAnswer(answer: Answer) {
     this.answerService.putAnswer(answer).subscribe(answer => {
-      const indexOfAnswer = this.answers.findIndex(
+      const answers = this._answers$.value;
+      const indexOfAnswer = answers.findIndex(
         a =>
           a.faccritId === answer.faccritId &&
           a.interviewId === answer.interviewId &&
           a.questionId === answer.questionId,
       );
 
-      this.answers[indexOfAnswer] = answer;
+      this._answers$.next([
+        ...answers.slice(0, indexOfAnswer),
+        answer,
+        ...answers.slice(indexOfAnswer + 1),
+      ]);
     });
   }
 }
