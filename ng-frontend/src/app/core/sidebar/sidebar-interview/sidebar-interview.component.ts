adac9de236c9e3c0f8110dd6dab45773b9ff9fb7
Oscar Rosner <oscar.rosner@web.de>
2020-07-19 19:42:33 +0200

    [REFACTOR] Use answer store in sidebar-interview component

--- ng-frontend/src/app/core/sidebar/sidebar-interview/sidebar-interview.component.ts
+++ ng-frontend/src/app/core/sidebar/sidebar-interview/sidebar-interview.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { Observable, combineLatest } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Select } from '@ngxs/store';
 import { NbMenuService, NbMenuItem } from '@nebular/theme';
-import { Question } from 'src/app/core/data/models/question.model';
 import { AppRouterState } from 'src/app/core/ngxs/app-router.state';
-import { InterviewState } from 'src/app/core/ngxs/interview.state';
-import { Answer } from 'src/app/core/data/models/answer.model';
-import { map, first } from 'rxjs/operators';
+import { map, filter } from 'rxjs/operators';
+import { AnswerStore } from '../../stores/answer.store';
 
 @Component({
   selector: 'app-sidebar-interview',
@@ -16,15 +14,13 @@ import { map, first } from 'rxjs/operators';
 export class SidebarInterviewComponent {
   @Select(AppRouterState.facCritId) facCritId$: Observable<number>;
   @Select(AppRouterState.interviewId) interviewId$: Observable<number>;
-  @Select(InterviewState.answers) answers$: Observable<Answer[]>;
-  @Select(InterviewState.questions) questions$: Observable<Question[]>;
 
   items: NbMenuItem[];
 
   questionIds: number[];
 
-  constructor(private menuService: NbMenuService) {}
-  
+  constructor(private menuService: NbMenuService, private answerStore: AnswerStore) {}
+
   ngOnInit() {
     this.items = [];
     this.menuService.onItemClick().subscribe(x => {
@@ -32,24 +28,18 @@ export class SidebarInterviewComponent {
       el?.scrollIntoView({ behavior: 'smooth', block: 'center' });
     });
 
-    combineLatest([this.facCritId$, this.interviewId$]).subscribe(ids => {
-      this.answers$
+    this.facCritId$.subscribe(id => {
+      this.answerStore.answers$
         .pipe(
-          first(answers => answers.length > 0),
-          map(answers => answers.filter(a => a.faccritId === ids[0] && a.interviewId === ids[1])),
+          filter(answers => answers != null),
+          map(answers => answers.filter(a => a.faccritId === id)),
         )
         .subscribe(answers => {
-          this.questionIds = answers.map(a => a.questionId);
+          this.items = answers.map<NbMenuItem>(a => ({
+            title: a.questionText,
+            data: a.questionId,
+          }));
         });
-
-      this.questions$.pipe(first(questions => questions.length > 0)).subscribe(questions => {
-        if (this.questionIds) {
-          this.items = questions
-            .filter(q => this.questionIds.includes(q.id) && q.facCritId === ids[0])
-            .sort((a, b) => a.id - b.id)
-            .map<NbMenuItem>(q => ({ title: q.textDe, data: q.id }));
-        }
-      });
     });
   }
 }
