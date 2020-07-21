1abe5f7b770e00da96130b1101a0a592fa06b5d9
Oscar Rosner <oscar.rosner@web.de>
2020-07-21 15:48:34 +0200

    [FIX] Add nullchecks for _audits$.value

--- ng-frontend/src/app/core/data/stores/audit.store.ts
+++ ng-frontend/src/app/core/data/stores/audit.store.ts
@@ -23,14 +23,22 @@ export class AuditStore {
   addAudit(audit: Audit): void {
     this.auditService.postAudit(audit).subscribe(audit => {
       const audits = this._audits$.value;
-      this._audits$.next([...audits, audit]);
+      if (!audits) {
+        return this._audits$.next([audit]);
+      }
+
+      return this._audits$.next([...audits, audit]);
     });
   }
 
   updateAudit(audit: Audit): void {
-    const oldAudit = this._audits$.value.find(a => a.id === audit.id);
+    const oldAudit = this._audits$.value?.find(a => a.id === audit.id);
+
+    if (!oldAudit) {
+      throw new Error("Couldn't find audit to update");
+    }
+
     this.auditService.putAudit(oldAudit, audit).subscribe(audit => {
-      console.log(audit);
       const audits = this._audits$.value;
       const indexOfUpdatedAudit = audits.findIndex(a => a.id === audit.id);
 
