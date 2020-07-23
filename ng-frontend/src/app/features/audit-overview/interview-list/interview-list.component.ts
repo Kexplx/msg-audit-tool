d600ee2998e6ac9b652b34fb3adc4423bc83381b
Oscar Rosner <oscar.rosner@web.de>
2020-07-23 12:59:44 +0200

    [FIX] Add first pipe to auditId$ subscription

--- ng-frontend/src/app/features/audit-overview/interview-list/interview-list.component.ts
+++ ng-frontend/src/app/features/audit-overview/interview-list/interview-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Audit } from 'src/app/core/data/models/audit.model';
 import { Interview, InterviewStatus } from 'src/app/core/data/models/interview.model';
-import { map } from 'rxjs/operators';
+import { map, first } from 'rxjs/operators';
 import { InterviewStore } from 'src/app/core/data/stores/interview.store';
 import { SubSink } from 'subsink';
 import { AuditStore } from 'src/app/core/data/stores/audit.store';
@@ -25,22 +25,18 @@ export class InterviewListComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
-    const idSub = this.idService.auditId$.subscribe(id => {
+    this.idService.auditId$.pipe(first()).subscribe(id => {
       const auditSub = this.auditStore.audits$
         .pipe(map(audits => audits?.find(a => a.id === id)))
         .subscribe(audit => (this.audit = audit));
 
       const interviewSub = this.interviewStore.interviews$
         .pipe(map(interviews => interviews?.filter(i => i.auditId === id)))
-        .subscribe(interviews => {
-          this.interviews = interviews;
-        });
+        .subscribe(interviews => (this.interviews = interviews));
 
       this.subSink.add(auditSub, interviewSub);
       this.interviewStore.loadInterviewsByAuditId(id);
     });
-
-    this.subSink.add(idSub);
   }
 
   ngOnDestroy() {
