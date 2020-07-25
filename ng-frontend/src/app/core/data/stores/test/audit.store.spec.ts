ad9e964821e1d14adcec6c955f1f6ee839786665
Oscar Rosner <oscar.rosner@web.de>
2020-07-25 23:46:25 +0200

    [TEST] Restructure tests using ..['..'] to access private fields

--- ng-frontend/src/app/core/data/stores/test/audit.store.spec.ts
+++ ng-frontend/src/app/core/data/stores/test/audit.store.spec.ts
@@ -1,24 +1,23 @@
 import { AuditService } from '../../http/audit.service';
 import { AUDITS } from '../../http/test/dummies/app-models/audits';
-import { of, timer } from 'rxjs';
+import { of } from 'rxjs';
 import { AuditStore } from '../audit.store';
-import { filter, first } from 'rxjs/operators';
+import { filter, skip } from 'rxjs/operators';
 import { Audit } from '../../models/audit.model';
-import { StoreActionService, StoreActionType } from '../store-action.service';
+import { StoreActionService } from '../store-action.service';
 
-/**
- * Unit tests for AuditStore.
- *
- * Dummy Data importet from ../../http/test/dummies/app-models/audits.
- */
-fdescribe('AuditStore', () => {
+describe('AuditStore', () => {
   let auditStore: AuditStore;
   let auditServiceSpy: jasmine.SpyObj<AuditService>;
   let storeActionServiceSpy: jasmine.SpyObj<StoreActionService>;
 
   beforeEach(() => {
     auditServiceSpy = jasmine.createSpyObj<AuditService>(['getAudits', 'postAudit', 'putAudit']);
-    storeActionServiceSpy = jasmine.createSpyObj<StoreActionService>(['notify']);
+    storeActionServiceSpy = jasmine.createSpyObj<StoreActionService>([
+      'notifyLoad',
+      'notifyAdd',
+      'notifyEdit',
+    ]);
 
     auditStore = new AuditStore(auditServiceSpy, storeActionServiceSpy);
   });
@@ -28,39 +27,33 @@ fdescribe('AuditStore', () => {
       auditServiceSpy.getAudits.and.returnValue(of(AUDITS));
     });
 
-    it('should call #notify once', () => {
-      auditStore.loadAudits();
-
-      expect(storeActionServiceSpy.notify.calls.count()).toEqual(1);
-    });
-
     it('should call #getAudits once', () => {
       auditStore.loadAudits();
 
       expect(auditServiceSpy.getAudits.calls.count()).toEqual(1);
     });
 
-    it('audits$ should emit the new array of audits', () => {
+    it('should set _audits$.value to the response', () => {
       auditStore.audits$.pipe(filter(audits => audits != null)).subscribe(audits => {
         expect(audits).toEqual(AUDITS);
       });
 
       auditStore.loadAudits();
     });
+
+    it('should call #notifyLoad once', () => {
+      auditStore.loadAudits();
+
+      expect(storeActionServiceSpy.notifyLoad.calls.count()).toEqual(1);
+    });
   });
 
   describe('#addAudit', () => {
-    let postAuditResponse: Audit;
+    let auditResponse: Audit;
 
     beforeEach(() => {
-      postAuditResponse = AUDITS[0];
-      auditServiceSpy.postAudit.and.returnValue(of(postAuditResponse));
-    });
-
-    it('should call #notify once', () => {
-      auditStore.addAudit({} as Audit);
-
-      expect(storeActionServiceSpy.notify.calls.count()).toEqual(1);
+      auditResponse = AUDITS[0];
+      auditServiceSpy.postAudit.and.returnValue(of(auditResponse));
     });
 
     it('should call #postAudit', () => {
@@ -69,46 +62,49 @@ fdescribe('AuditStore', () => {
       expect(auditServiceSpy.postAudit.calls.count()).toEqual(1);
     });
 
-    it('audits$ should emit the previous audits and the new one', () => {
+    it('should add the added audit to _audits$.value', () => {
       auditStore.audits$.pipe(filter(audits => audits != null)).subscribe(audits => {
-        const lastIndex = audits.length - 1;
-        expect(audits[lastIndex]).toEqual(postAuditResponse);
+        expect(audits[0]).toEqual(auditResponse);
       });
 
       auditStore.addAudit({} as Audit);
     });
+
+    it('should call #notifyAdd once', () => {
+      auditStore.addAudit({} as Audit);
+
+      expect(storeActionServiceSpy.notifyAdd.calls.count()).toEqual(1);
+    });
   });
 
   describe('#updateAudits', () => {
-    let putAuditResponse: Audit;
+    let auditResponse: Audit;
 
     beforeEach(() => {
-      putAuditResponse = AUDITS[1];
-      auditServiceSpy.getAudits.and.returnValue(of(AUDITS));
-      auditServiceSpy.putAudit.and.returnValue(of(putAuditResponse));
+      auditResponse = AUDITS[0];
+
+      auditStore['_audits$'].next([{ ...auditResponse, name: 'OLD VALUE' }]);
+      auditServiceSpy.putAudit.and.returnValue(of(auditResponse));
     });
 
-    it('should call #putAudit and #notify', () => {
-      auditStore.audits$
-        .pipe(
-          filter(audits => audits != null),
-          first(), // Take only first to not get into endless loop.
-        )
-        .subscribe(audits => {
-          const auditToUpdate = audits[0];
-          auditStore.updateAudit(auditToUpdate);
+    it('should call #postAudit once', () => {
+      auditStore.updateAudit({} as Audit);
 
-          expect(auditServiceSpy.putAudit.calls.count()).toEqual(1);
-          expect(storeActionServiceSpy.notify.calls.count()).toEqual(1);
-        });
-      auditStore.loadAudits(); // load some audits into _audits.value.
+      expect(auditServiceSpy.putAudit.calls.count()).toEqual(1);
     });
 
-    it('should throw error if _audits$.value is null', () => {
-      // _audits$.value is null.
-      expect(() => {
-        auditStore.updateAudit({} as Audit);
-      }).toThrowError("Couldn't find audit to update");
+    it("should update the audit's name at index 0", () => {
+      auditStore.audits$.pipe(skip(1)).subscribe(contactPersons => {
+        expect(contactPersons[0].name).not.toEqual('OLD VALUE');
+      });
+
+      auditStore.updateAudit({} as Audit);
+    });
+
+    it('should call #notifyEdit once', () => {
+      auditStore.updateAudit({} as Audit);
+
+      expect(storeActionServiceSpy.notifyEdit.calls.count()).toEqual(1);
     });
   });
 });
