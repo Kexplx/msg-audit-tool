dd240eba975b7a6ac558a523997c984440fa549e
Oscar Rosner <oscar.rosner@web.de>
2020-07-27 04:19:05 +0200

    [FIX] Delete unused AnswersByFacCritId pipe

--- ng-frontend/src/app/features/interview/interview.module.ts
+++ ng-frontend/src/app/features/interview/interview.module.ts
@@ -5,7 +5,6 @@ import { InterviewComponent } from './interview.component';
 import { AnswerQuestionListComponent } from './answer-question-list/answer-question-list.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { AnswersByFacCritId } from './answers-by-faccrit-id.pipe';
 import { AddInterviewDialogComponent } from './add-interview-dialog/add-interview-dialog.component';
 import { InterviewFormComponent } from './interview-form/interview-form.component';
 import { ByIdPipe } from './by-id.pipe';
@@ -14,7 +13,6 @@ import { ByIdPipe } from './by-id.pipe';
   declarations: [
     InterviewComponent,
     AnswerQuestionListComponent,
-    AnswersByFacCritId,
     InterviewFormComponent,
     AddInterviewDialogComponent,
     ByIdPipe,
