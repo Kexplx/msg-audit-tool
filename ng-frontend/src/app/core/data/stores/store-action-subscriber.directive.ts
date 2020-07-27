831f215c47557b5009c379d54a6edd19b0e30ebc
Oscar Rosner <oscar.rosner@web.de>
2020-07-27 04:34:08 +0200

    [REFACTOR] Use switch-case instead of ternary operators

--- ng-frontend/src/app/core/data/stores/store-action-subscriber.directive.ts
+++ ng-frontend/src/app/core/data/stores/store-action-subscriber.directive.ts
@@ -1,9 +1,5 @@
 import { Directive, OnInit } from '@angular/core';
-import {
-  StoreActionService,
-  StoreAction,
-  StoreActionType,
-} from './store-action.service';
+import { StoreActionService, StoreAction, StoreActionType } from './store-action.service';
 import { NbToastrService } from '@nebular/theme';
 
 @Directive({
@@ -19,15 +15,21 @@ export class StoreActionSubscriberDirective implements OnInit {
     this.storeActionService.storeAction$.subscribe(storeAction => this.showToast(storeAction));
   }
 
-  private showToast(storeAction: StoreAction) {
-    const { message, type } = storeAction;
-
-    const icon =
-      type === StoreActionType.Load
-        ? 'download-outline'
-        : type === StoreActionType.Edit
-        ? 'edit-outline'
-        : 'file-add-outline';
+  private showToast({ message, type }: StoreAction): void {
+    let icon: string;
+    switch (type) {
+      case StoreActionType.Load:
+        icon = 'download-outline';
+        break;
+      case StoreActionType.Edit:
+        icon = 'edit-outline';
+        break;
+      case StoreActionType.Add:
+        icon = 'file-add-outline';
+        break;
+      default:
+        icon = 'question-mark-outline';
+    }
 
     this.toastService.show(message, 'Erfolg', { icon, position: 'bottom-end' as any });
   }
